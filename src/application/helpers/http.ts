4261411796e04ea13bb26130131d303b4f1a7ef2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 18:01:22 -0300

    feat: add a helper http method for statusCode 401

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,10 +1,15 @@
-import { ForbiddenError } from '@/application/errors'
+import { ForbiddenError, UnauthorizedError } from '@/application/errors'
 
 export type HttpResponse<T = any> = { statusCode: number, data: T }
 
 export const ok = <T = any> (data: T): HttpResponse<T> => ({
   statusCode: 200,
   data
+})
+
+export const unauthorized = (): HttpResponse<Error> => ({
+  statusCode: 401,
+  data: new UnauthorizedError()
 })
 
 export const forbidden = (): HttpResponse<Error> => ({
