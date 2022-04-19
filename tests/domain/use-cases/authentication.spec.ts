4027829a27a143e9b31d536fa4bc3a1653c60d9b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 02:45:13 -0300

    test: ensure Authentication return undefined if loadAccountByEmailRepository return undefined

--- tests/domain/use-cases/authentication.spec.ts
+++ tests/domain/use-cases/authentication.spec.ts
@@ -4,15 +4,22 @@ import { Authentication, setupAuthentication } from '@/domain/use-cases'
 import { mock, MockProxy } from 'jest-mock-extended'
 import faker from 'faker'
 
-describe('AddAccount', () => {
+describe('Authentication', () => {
   let sut: Authentication
+  let id: string
+  let name: string
   let email: string
+  let password: string
   let loadAccountByEmailRepository: MockProxy<LoadAccountByEmailRepository>
 
   beforeAll(() => {
+    id = faker.datatype.uuid()
+    name = faker.name.findName()
     email = faker.internet.email()
+    password = faker.internet.password(8)
 
     loadAccountByEmailRepository = mock()
+    loadAccountByEmailRepository.loadByEmail.mockResolvedValue({ id, name, email, password })
   })
 
   beforeEach(() => {
@@ -24,5 +31,13 @@ describe('AddAccount', () => {
 
     expect(loadAccountByEmailRepository.loadByEmail).toHaveBeenCalledWith({ email })
     expect(loadAccountByEmailRepository.loadByEmail).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return undefined if LoadAccountByEmailRepository return undefined', async () => {
+    loadAccountByEmailRepository.loadByEmail.mockResolvedValueOnce(undefined)
+
+    const account = await sut({ email })
+
+    expect(account).toBeUndefined()
   })
 })
