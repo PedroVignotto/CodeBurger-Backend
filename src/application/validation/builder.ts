ee0753e7aac5823fd7a3fa8639512147db314ab9
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 18:58:12 -0300

    feat: ensure ValidationBuilder return a Required validation if required() is call

---
+++ src/application/validation/builder.ts
@@ -0,0 +1,23 @@
+import { Required, Validator } from '@/application/validation'
+
+export class ValidationBuilder {
+  private constructor (
+    private readonly value: any,
+    private readonly fieldName: string,
+    private readonly validators: Validator[] = []
+  ) {}
+
+  static of (value: any, fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(value, fieldName)
+  }
+
+  required (): ValidationBuilder {
+    this.validators.push(new Required(this.value, this.fieldName))
+
+    return this
+  }
+
+  build (): Validator[] {
+    return this.validators
+  }
+}
--- src/application/validation/index.ts
+++ src/application/validation/index.ts
@@ -1,3 +1,4 @@
 export * from './composite'
+export * from './builder'
 export * from './required'
 export * from './validator'
