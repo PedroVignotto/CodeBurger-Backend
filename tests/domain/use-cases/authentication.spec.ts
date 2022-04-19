5a249c1c167496cc2fead22d1b9bf48387189723
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 03:01:22 -0300

    test: ensure Authentication call HashComparer with correct values

--- tests/domain/use-cases/authentication.spec.ts
+++ tests/domain/use-cases/authentication.spec.ts
@@ -1,3 +1,4 @@
+import { HashComparer } from '@/domain/contracts/gateways'
 import { LoadAccountByEmailRepository } from '@/domain/contracts/repositories'
 import { Authentication, setupAuthentication } from '@/domain/use-cases'
 
@@ -10,26 +11,30 @@ describe('Authentication', () => {
   let name: string
   let email: string
   let password: string
+  let hashedPassword: string
   let error: string
   let loadAccountByEmailRepository: MockProxy<LoadAccountByEmailRepository>
+  let hashComparer: MockProxy<HashComparer>
 
   beforeAll(() => {
     id = faker.datatype.uuid()
     name = faker.name.findName()
     email = faker.internet.email()
     password = faker.internet.password(8)
+    hashedPassword = faker.internet.password(16)
     error = faker.random.word()
 
     loadAccountByEmailRepository = mock()
-    loadAccountByEmailRepository.loadByEmail.mockResolvedValue({ id, name, email, password })
+    loadAccountByEmailRepository.loadByEmail.mockResolvedValue({ id, name, email, password: hashedPassword })
+    hashComparer = mock()
   })
 
   beforeEach(() => {
-    sut = setupAuthentication(loadAccountByEmailRepository)
+    sut = setupAuthentication(loadAccountByEmailRepository, hashComparer)
   })
 
   it('Should call LoadAccountByEmailRepository with correct email', async () => {
-    await sut({ email })
+    await sut({ email, password })
 
     expect(loadAccountByEmailRepository.loadByEmail).toHaveBeenCalledWith({ email })
     expect(loadAccountByEmailRepository.loadByEmail).toHaveBeenCalledTimes(1)
@@ -38,7 +43,7 @@ describe('Authentication', () => {
   it('Should return undefined if LoadAccountByEmailRepository return undefined', async () => {
     loadAccountByEmailRepository.loadByEmail.mockResolvedValueOnce(undefined)
 
-    const account = await sut({ email })
+    const account = await sut({ email, password })
 
     expect(account).toBeUndefined()
   })
@@ -46,8 +51,15 @@ describe('Authentication', () => {
   it('Should rethrow if LoadAccountByEmailRepository throws', async () => {
     loadAccountByEmailRepository.loadByEmail.mockRejectedValueOnce(new Error(error))
 
-    const promise = sut({ email })
+    const promise = sut({ email, password })
 
     await expect(promise).rejects.toThrow(new Error(error))
+  })
+
+  it('Should call HashComparer with correct values', async () => {
+    await sut({ email, password })
+
+    expect(hashComparer.compare).toHaveBeenCalledWith({ plaintext: password, digest: hashedPassword })
+    expect(hashComparer.compare).toHaveBeenCalledTimes(1)
   })
 })
