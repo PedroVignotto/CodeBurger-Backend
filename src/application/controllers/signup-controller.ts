4f4bb413964da0f7216e657818d7cac3f2c52675
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 17:08:32 -0300

    feat: ensure SignUpController return forbidden if addAccount return false

--- src/application/controllers/signup-controller.ts
+++ src/application/controllers/signup-controller.ts
@@ -1,13 +1,16 @@
-import { HttpResponse, ok } from '@/application/helpers'
+import { forbidden, HttpResponse, ok } from '@/application/helpers'
 import { AddAccount } from '@/domain/use-cases'
 
 type HttpRequest = { name: string, email: string, password: string, passwordConfirmation: string }
-type Model = undefined
+type Model = undefined | Error
 
 export class SignUpController {
   constructor (private readonly addAccount: AddAccount) {}
+
   async perform ({ name, email, password }: HttpRequest): Promise<HttpResponse<Model>> {
-    await this.addAccount({ name, email, password })
+    const created = await this.addAccount({ name, email, password })
+
+    if (!created) return forbidden()
 
     return ok(undefined)
   }
