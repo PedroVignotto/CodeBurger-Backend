64994375688c72bd9d6096b0932fc106669ce042
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 19:10:39 -0300

    feat: ensure SignUpController return badRequest if any validation fails

--- src/application/controllers/signup-controller.ts
+++ src/application/controllers/signup-controller.ts
@@ -1,4 +1,5 @@
-import { forbidden, HttpResponse, created, unauthorized } from '@/application/helpers'
+import { forbidden, HttpResponse, created, unauthorized, badRequest } from '@/application/helpers'
+import { ValidationBuilder as Builder, ValidationComposite } from '@/application/validation'
 import { AddAccount, Authentication } from '@/domain/use-cases'
 
 type HttpRequest = { name: string, email: string, password: string, passwordConfirmation: string }
@@ -7,7 +8,11 @@ type Model = { name: string, accessToken: string } | Error
 export class SignUpController {
   constructor (private readonly addAccount: AddAccount, private readonly authentication: Authentication) {}
 
-  async perform ({ name, email, password }: HttpRequest): Promise<HttpResponse<Model>> {
+  async perform ({ name, email, password, passwordConfirmation }: HttpRequest): Promise<HttpResponse<Model>> {
+    const error = this.validate({ name, email, password, passwordConfirmation })
+
+    if (error) return badRequest(error)
+
     const account = await this.addAccount({ name, email, password })
 
     if (!account) return forbidden()
@@ -17,5 +22,14 @@ export class SignUpController {
     if (!data) return unauthorized()
 
     return created(data)
+  }
+
+  private validate ({ name, email, password, passwordConfirmation }: HttpRequest): Error | undefined {
+    return new ValidationComposite([
+      ...Builder.of(name, 'name').required().build(),
+      ...Builder.of(email, 'email').required().build(),
+      ...Builder.of(password, 'password').required().build(),
+      ...Builder.of(passwordConfirmation, 'passwordConfirmation').required().build()
+    ]).validate()
   }
 }
