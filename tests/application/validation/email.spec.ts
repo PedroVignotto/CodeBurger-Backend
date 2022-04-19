03e939d05ade3c2e98bfdd9bd47304b5a409a09e
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 20:48:54 -0300

    test: ensure Email return InvalidFieldError if email is invalid

---
+++ tests/application/validation/email.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidFieldError } from '@/application/errors'
+import { Email } from '@/application/validation'
+
+import faker from 'faker'
+
+describe('Email', () => {
+  let email: string
+  let fieldName: string
+
+  beforeEach(() => {
+    email = faker.internet.email()
+    fieldName = faker.database.column()
+  })
+
+  test('Should return InvalidFieldError if email is invalid', () => {
+    const sut = new Email(email, fieldName)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidFieldError(fieldName))
+  })
+})
