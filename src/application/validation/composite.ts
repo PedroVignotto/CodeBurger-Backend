2db398c412f4b73aee0cbf1f5fffa84a9b99c11b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 18:50:31 -0300

    feat: ensure ValidationComposite return the first error if any validator fails

--- src/application/validation/composite.ts
+++ src/application/validation/composite.ts
@@ -1,9 +1,13 @@
 import { Validator } from '@/application/validation'
 
-export class ValidationComposite {
+export class ValidationComposite implements Validator {
   constructor (private readonly validators: Validator[]) {}
 
-  validate (): undefined {
-    return undefined
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+
+      if (error) return error
+    }
   }
 }
