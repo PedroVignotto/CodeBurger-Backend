8057d490782e87ad845e93af78699e99b1924ba1
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 19:45:42 -0300

    feat: add Controller template

---
+++ src/application/controllers/controller.ts
@@ -0,0 +1,24 @@
+import { badRequest, HttpResponse, serverError } from '@/application/helpers'
+import { ValidationComposite, Validator } from '@/application/validation'
+
+export abstract class Controller {
+  abstract perform (httpRequest: any): Promise<HttpResponse>
+
+  buildValidators (httpRequest: any): Validator[] { return [] }
+
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    const error = this.validate(httpRequest)
+
+    if (error) return badRequest(error)
+
+    try {
+      return await this.perform(httpRequest)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+
+  private validate (httpRequest: any): Error | undefined {
+    return new ValidationComposite(this.buildValidators(httpRequest)).validate()
+  }
+}
