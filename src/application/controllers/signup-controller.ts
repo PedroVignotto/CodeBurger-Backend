947d6bdff3a5a3d5a5712c5a9a117d15757bc0f5
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 17:12:47 -0300

    feat: ensure SignUpController call authentication with correct values

--- src/application/controllers/signup-controller.ts
+++ src/application/controllers/signup-controller.ts
@@ -1,16 +1,18 @@
 import { forbidden, HttpResponse, ok } from '@/application/helpers'
-import { AddAccount } from '@/domain/use-cases'
+import { AddAccount, Authentication } from '@/domain/use-cases'
 
 type HttpRequest = { name: string, email: string, password: string, passwordConfirmation: string }
 type Model = undefined | Error
 
 export class SignUpController {
-  constructor (private readonly addAccount: AddAccount) {}
+  constructor (private readonly addAccount: AddAccount, private readonly authentication: Authentication) {}
 
   async perform ({ name, email, password }: HttpRequest): Promise<HttpResponse<Model>> {
     const created = await this.addAccount({ name, email, password })
 
     if (!created) return forbidden()
+
+    await this.authentication({ email, password })
 
     return ok(undefined)
   }
