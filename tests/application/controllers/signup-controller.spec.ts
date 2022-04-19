6e1b8bf53e94c009f76aa612c7f7d83e588336a6
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 19:54:02 -0300

    test: apply controller template

--- tests/application/controllers/signup-controller.spec.ts
+++ tests/application/controllers/signup-controller.spec.ts
@@ -1,11 +1,8 @@
-import { SignUpController } from '@/application/controllers'
-import { ForbiddenError, ServerError, UnauthorizedError } from '@/application/errors'
+import { Controller, SignUpController } from '@/application/controllers'
+import { Required } from '@/application/validation'
+import { ForbiddenError, UnauthorizedError } from '@/application/errors'
 
-import { mocked } from 'jest-mock'
 import faker from 'faker'
-import { Required, ValidationComposite } from '@/application/validation'
-
-jest.mock('@/application/validation/composite')
 
 describe('SignUpController', () => {
   let sut: SignUpController
@@ -16,7 +13,6 @@ describe('SignUpController', () => {
   let passwordConfirmation: string
   let hashedPassword: string
   let accessToken: string
-  let error: string
 
   let addAccount: jest.Mock
   let authentication: jest.Mock
@@ -29,7 +25,6 @@ describe('SignUpController', () => {
     hashedPassword = faker.internet.password(16)
     passwordConfirmation = password
     accessToken = faker.datatype.uuid()
-    error = faker.random.word()
 
     addAccount = jest.fn()
     addAccount.mockResolvedValue({ id, name, email, password: hashedPassword })
@@ -41,26 +36,23 @@ describe('SignUpController', () => {
     sut = new SignUpController(addAccount, authentication)
   })
 
-  it('Should return badRequest if any validation fails', async () => {
-    const validations = [
+  it('Should extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ name, email, password, passwordConfirmation })
+
+    expect(validators).toEqual([
       new Required(name, 'name'),
       new Required(email, 'email'),
       new Required(password, 'password'),
       new Required(passwordConfirmation, 'passwordConfirmation')
-    ]
-    mocked(ValidationComposite).mockImplementationOnce(
-      jest.fn().mockImplementationOnce(() => ({ validate: jest.fn().mockReturnValueOnce(new Error(error)) }))
-    )
-
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
-
-    expect(ValidationComposite).toHaveBeenCalledWith(validations)
-    expect(statusCode).toBe(400)
-    expect(data).toEqual(new Error(error))
+    ])
   })
 
   it('Should call addAccount with correct values', async () => {
-    await sut.perform({ name, email, password, passwordConfirmation })
+    await sut.handle({ name, email, password, passwordConfirmation })
 
     expect(addAccount).toHaveBeenCalledWith({ name, email, password })
     expect(addAccount).toHaveBeenCalledTimes(1)
@@ -69,14 +61,14 @@ describe('SignUpController', () => {
   it('Should return forbidden if addAccount return false', async () => {
     addAccount.mockResolvedValueOnce(false)
 
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
+    const { statusCode, data } = await sut.handle({ name, email, password, passwordConfirmation })
 
     expect(statusCode).toBe(403)
     expect(data).toEqual(new ForbiddenError())
   })
 
   it('Should call authentication with correct values', async () => {
-    await sut.perform({ name, email, password, passwordConfirmation })
+    await sut.handle({ name, email, password, passwordConfirmation })
 
     expect(authentication).toHaveBeenCalledWith({ email, password })
     expect(authentication).toHaveBeenCalledTimes(1)
@@ -85,34 +77,16 @@ describe('SignUpController', () => {
   it('Should return unauthorized if authentication return undefined', async () => {
     authentication.mockResolvedValueOnce(undefined)
 
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
+    const { statusCode, data } = await sut.handle({ name, email, password, passwordConfirmation })
 
     expect(statusCode).toBe(401)
     expect(data).toEqual(new UnauthorizedError())
   })
 
   it('Should return created if valid data is provided', async () => {
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
+    const { statusCode, data } = await sut.handle({ name, email, password, passwordConfirmation })
 
     expect(statusCode).toBe(201)
     expect(data).toEqual({ name, accessToken })
-  })
-
-  it('Should returns serverError if have any throw', async () => {
-    authentication.mockRejectedValueOnce(new Error(error))
-
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
-
-    expect(statusCode).toBe(500)
-    expect(data).toEqual(new ServerError(new Error(error)))
-  })
-
-  it('Should returns serverError if have any throw a non error object', async () => {
-    authentication.mockRejectedValueOnce(error)
-
-    const { statusCode, data } = await sut.perform({ name, email, password, passwordConfirmation })
-
-    expect(statusCode).toBe(500)
-    expect(data).toEqual(new ServerError())
   })
 })
