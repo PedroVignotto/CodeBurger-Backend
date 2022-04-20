05fca556c62e13c1c3a033d34bcf01cdbbe26216
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 23:30:04 -0300

    feat: ensure BcryptAdapter call compare with correct values

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -1,9 +1,13 @@
-import { HashGenerator } from '@/domain/contracts/gateways'
+import { HashComparer, HashGenerator } from '@/domain/contracts/gateways'
 
 import bcrypt from 'bcryptjs'
 
-export class BCryptAdapter implements HashGenerator {
+export class BCryptAdapter implements HashGenerator, HashComparer {
   async generate ({ plaintext }: HashGenerator.Input): Promise<HashGenerator.Output> {
     return bcrypt.hash(plaintext, 12)
+  }
+
+  async compare ({ plaintext, digest }: HashComparer.Input): Promise<HashComparer.Output> {
+    return bcrypt.compare(plaintext, digest)
   }
 }
