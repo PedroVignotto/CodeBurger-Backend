90815ad1bfd4f47ef144458f9054f6b57b9d432a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-19 21:26:51 -0300

    test: ensure CompareValidation return undefined if fields are equal

--- tests/application/validation/compare.spec.ts
+++ tests/application/validation/compare.spec.ts
@@ -5,18 +5,30 @@ import faker from 'faker'
 
 describe('CompareValidation', () => {
   let field: string
+  let fieldToCompareValid: string
+  let fieldToCompareInvalid: string
   let fieldToCompareName: string
 
   beforeAll(() => {
     field = faker.random.words(1)
+    fieldToCompareValid = field
+    fieldToCompareInvalid = faker.random.words(2)
     fieldToCompareName = faker.database.column()
   })
 
   test('Should return InvalidFieldError if fields are not equal', () => {
-    const sut = new CompareValidation(field, faker.random.words(2), fieldToCompareName)
+    const sut = new CompareValidation(field, fieldToCompareInvalid, fieldToCompareName)
 
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidFieldError(fieldToCompareName))
+  })
+
+  it('Should return undefined if fields are equal', () => {
+    const sut = new CompareValidation(field, fieldToCompareValid, fieldToCompareName)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
