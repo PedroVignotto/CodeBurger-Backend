45fce25a98553ce032c6d1523412600ca5d152a6
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-20 03:35:18 -0300

    test: ensure JwtAdapter rethrow if sign throw

--- tests/infra/gateways/jwt-adapter.spec.ts
+++ tests/infra/gateways/jwt-adapter.spec.ts
@@ -10,12 +10,14 @@ describe('JwtAdapter', () => {
   let secret: string
   let key: string
   let token: string
+  let error: Error
   let fakeJwt = jwt as jest.Mocked<typeof jwt>
 
   beforeAll(() => {
     secret = faker.datatype.uuid()
     key = faker.random.word()
     token = faker.datatype.uuid()
+    error = new Error(faker.random.word())
 
     fakeJwt = jwt as jest.Mocked<typeof jwt>
     fakeJwt.sign.mockImplementation(() => token)
@@ -30,6 +32,14 @@ describe('JwtAdapter', () => {
 
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: '1d' })
     expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should rethrow if sign throw', async () => {
+    fakeJwt.sign.mockImplementationOnce(() => { throw error })
+
+    const promise = sut.generate({ key })
+
+    await expect(promise).rejects.toThrow(error)
   })
 
   it('Should return a accessToken on success', async () => {
