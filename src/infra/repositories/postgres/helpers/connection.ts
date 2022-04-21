e573bd5c1e66b407598ec359d8e7329ac5fe184c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 01:06:22 -0300

    feat: ensure PgConnection return ConnectionNotFoundError on disconnect if connection is not found

--- src/infra/repositories/postgres/helpers/connection.ts
+++ src/infra/repositories/postgres/helpers/connection.ts
@@ -1,20 +1,28 @@
-import { createConnection, getConnection, getConnectionManager } from 'typeorm'
+import { ConnectionNotFoundError } from '@/infra/repositories/postgres/errors'
+
+import { Connection, createConnection, getConnection, getConnectionManager } from 'typeorm'
 
 export class PgConnection {
   private static instance?: PgConnection
+  private connection?: Connection
 
   private constructor () {}
 
   static getInstance (): PgConnection {
     if (!PgConnection.instance) PgConnection.instance = new PgConnection()
+
     return PgConnection.instance
   }
 
   async connect (): Promise<void> {
-    getConnectionManager().has('default') ? getConnection() : await createConnection()
+    this.connection = getConnectionManager().has('default') ? getConnection() : await createConnection()
   }
 
   async disconnect (): Promise<void> {
+    if (!this.connection) throw new ConnectionNotFoundError()
+
     await getConnection().close()
+
+    this.connection = undefined
   }
 }
