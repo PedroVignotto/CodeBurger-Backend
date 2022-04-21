9d21371581377cdc33bc0d11e826ed533d51e10a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 07:46:55 -0300

    feat: ensure requests return json as default

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { bodyParser, cors } from '@/main/middlewares'
+import { bodyParser, cors, contentType } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const contentType = (req: Request, res: Response, next: NextFunction): void => {
+  res.type('json')
+
+  next()
+}
