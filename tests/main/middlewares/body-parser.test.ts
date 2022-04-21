4a9193fc79c803ff2248dbd0a79e8420082f952c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 07:35:45 -0300

    test: ensure requests parse body as json

---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,18 @@
+import { app } from '@/main/config/app'
+
+import request from 'supertest'
+import faker from 'faker'
+
+describe('BodyParser Middleware', () => {
+  let name: string
+
+  beforeEach(() => {
+    name = faker.name.findName()
+  })
+
+  it('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => { res.send(req.body) })
+
+    await request(app).post('/test_body_parser').send({ name }).expect({ name })
+  })
+})
