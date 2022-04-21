b6bfd1a1c0b6c13871d2e00912d32d083602d2d5
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 01:06:47 -0300

    test: ensure PgConnection return ConnectionNotFoundError on disconnect if connection is not found

--- tests/infra/repositories/postgres/helpers/connection.spec.ts
+++ tests/infra/repositories/postgres/helpers/connection.spec.ts
@@ -2,6 +2,7 @@ import { PgConnection } from '@/infra/repositories/postgres/helpers'
 
 import { createConnection, getConnection, getConnectionManager } from 'typeorm'
 import { mocked } from 'jest-mock'
+import { ConnectionNotFoundError } from '@/infra/repositories/postgres/errors'
 
 jest.mock('typeorm', () => ({
   Entity: jest.fn(),
@@ -62,5 +63,12 @@ describe('PgConnection', () => {
 
     expect(closeSpy).toHaveBeenCalledWith()
     expect(closeSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return ConnectionNotFoundError on disconnect if connection is not found', async () => {
+    const promise = sut.disconnect()
+
+    expect(closeSpy).not.toHaveBeenCalled()
+    await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
   })
 })
