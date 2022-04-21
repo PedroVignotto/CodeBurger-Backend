87e85113cde61f5a713dec43b8ef78745e423836
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 03:29:09 -0300

    test: ensure UuidAdapter return uuid

--- tests/infra/gateways/uuid-adapter.spec.ts
+++ tests/infra/gateways/uuid-adapter.spec.ts
@@ -1,19 +1,33 @@
 import { UuidAdapter } from '@/infra/gateways'
 
-import { v4 as uuid } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
+import { mocked } from 'jest-mock'
+import faker from 'faker'
 
 jest.mock('uuid')
 
 describe('UuidAdapter', () => {
+  let uuid: string
   let sut: UuidAdapter
+
+  beforeAll(() => {
+    uuid = faker.datatype.uuid()
+    mocked(uuidV4).mockReturnValue(uuid)
+  })
 
   beforeEach(() => {
     sut = new UuidAdapter()
   })
 
-  it('Should call uuid', () => {
+  it('Should call uuidV4', () => {
     sut.generate()
 
-    expect(uuid).toHaveBeenCalledTimes(1)
+    expect(uuidV4).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return uuid', () => {
+    const generatedUUID = sut.generate()
+
+    expect(generatedUUID).toBe(uuid)
   })
 })
