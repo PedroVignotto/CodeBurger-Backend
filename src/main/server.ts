4f45e0ba8f5db613d88714ec1f4efce0cb84b457
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 07:15:58 -0300

    chore: add express

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,8 @@
+import './config/module-alias'
+
 import 'reflect-metadata'
+import express from 'express'
+
+const app = express()
+
+app.listen(3333, () => console.log('Server is running at http://localhost:3333'))
