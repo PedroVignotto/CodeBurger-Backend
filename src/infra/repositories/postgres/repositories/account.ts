a4b5c469dff996e4fb892d605c18a4444dce1c99
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 04:35:33 -0300

    feat: ensure AccountRepository return a account on success

--- src/infra/repositories/postgres/repositories/account.ts
+++ src/infra/repositories/postgres/repositories/account.ts
@@ -1,13 +1,24 @@
 import { Account } from '@/infra/repositories/postgres/entities'
 import { PgRepository } from '@/infra/repositories/postgres/repositories'
-import { CheckAccountByEmailRepository } from '@/domain/contracts/repositories'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/domain/contracts/repositories'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
 
-export class AccountRepository extends PgRepository implements CheckAccountByEmailRepository {
+export class AccountRepository extends PgRepository implements CheckAccountByEmailRepository, AddAccountRepository {
+  constructor (private readonly uuid: UUIDGenerator) { super() }
+
   async checkByEmail ({ email }: CheckAccountByEmailRepository.Input): Promise<CheckAccountByEmailRepository.Output> {
     const repository = this.getRepository(Account)
 
     const emailExists = await repository.findOne({ email })
 
     return !!emailExists
+  }
+
+  async create ({ name, email, password }: AddAccountRepository.Input): Promise<AddAccountRepository.Output> {
+    const repository = this.getRepository(Account)
+
+    const account = await repository.save({ id: this.uuid.generate(), name, email, password })
+
+    return account
   }
 }
