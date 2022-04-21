a4d91d6f2078828a10b505064c378451ce29ed1a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 07:56:46 -0300

    refactor: add env on config

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import './config/module-alias'
-import { app } from '@/main/config/app'
+import { app, env } from '@/main/config'
 
 import 'reflect-metadata'
 
-app.listen(3333, () => console.log('Server is running at http://localhost:3333'))
+app.listen(env.port, () => console.log(`Server is running at http://localhost:${env.port}`))
