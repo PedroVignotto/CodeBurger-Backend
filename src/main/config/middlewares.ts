d79a711d5dd9e61e4471f238d3ea9314bebf7976
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 07:39:36 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
-import { bodyParser } from '@/main/middlewares'
+import { bodyParser, cors } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-headers', '*')
+  res.set('access-control-allow-methods', '*')
+
+  next()
+}
