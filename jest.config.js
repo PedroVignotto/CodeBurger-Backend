aa66cc8f39ecfcf7e4fd3d43aca617e073f1530b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 17:04:03 -0300

    chore: add middlewares to coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
+    '<rootDir>/src/main/middlewares/**',
     '!<rootDir>/src/**/index.ts'
   ],
   coverageDirectory: 'coverage',
