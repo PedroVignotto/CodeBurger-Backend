3be1dcd459778002c04a2f18a0f64c4fd8db3868
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-22 07:40:15 -0300

    test: ensure LoginController return unauthorized if authentication return undefine d

--- tests/application/controllers/login.spec.ts
+++ tests/application/controllers/login.spec.ts
@@ -1,4 +1,5 @@
 import { Controller, LoginController } from '@/application/controllers'
+import { UnauthorizedError } from '@/application/errors'
 import { EmailValidation, RequiredValidation } from '@/application/validation'
 
 import faker from 'faker'
@@ -6,16 +7,22 @@ import faker from 'faker'
 describe('LoginController', () => {
   let sut: LoginController
 
+  let name: string
   let email: string
   let password: string
+  let accessToken: string
 
   const authentication: jest.Mock = jest.fn()
 
   beforeEach(() => {
     sut = new LoginController(authentication)
 
+    name = faker.name.findName()
     email = faker.internet.email()
     password = faker.internet.password(8)
+    accessToken = faker.datatype.uuid()
+
+    authentication.mockResolvedValue({ name, accessToken })
   })
 
   it('Should extend Controller', async () => {
@@ -37,5 +44,14 @@ describe('LoginController', () => {
 
     expect(authentication).toHaveBeenCalledWith({ email, password })
     expect(authentication).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return unauthorized if authentication return undefined', async () => {
+    authentication.mockResolvedValueOnce(undefined)
+
+    const { statusCode, data } = await sut.handle({ email, password })
+
+    expect(statusCode).toBe(401)
+    expect(data).toEqual(new UnauthorizedError())
   })
 })
