5c60075a443777272b61f8aace0666044088a8a6
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 23:24:22 -0300

    feat: add migration to accounts table

---
+++ src/infra/database/postgres/helpers/migrations/1650592833124-CreateAccounts.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateAccounts1650592833124 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('accounts')
+  }
+}
