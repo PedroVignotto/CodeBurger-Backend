274eccbd17ff2e18f7cd0ae9f7d9120ac9ef9e9f
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-22 02:20:53 -0300

    feat: apply no-cache middleware on docs

--- src/main/config/docs.ts
+++ src/main/config/docs.ts
@@ -1,8 +1,9 @@
+import { noCache } from '@/main/middlewares'
 import { swagger } from '@/main/docs'
 
 import { serve, setup } from 'swagger-ui-express'
 import { Express } from 'express'
 
 export const setupDocs = (app: Express): void => {
-  app.use('/api-docs', serve, setup(swagger))
+  app.use('/api-docs', noCache, serve, setup(swagger))
 }
