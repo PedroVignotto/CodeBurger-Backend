43c3b9b9b746f1e0e0376f19278c0a529e093cd9
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-22 07:28:12 -0300

    feat: add a helper http method for statusCode 200

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -2,6 +2,11 @@ import { ForbiddenError, ServerError } from '@/application/errors'
 
 export type HttpResponse<T = any> = { statusCode: number, data: T }
 
+export const ok = <T = any> (data: T): HttpResponse<T> => ({
+  statusCode: 200,
+  data
+})
+
 export const created = <T = any> (data: T): HttpResponse<T> => ({
   statusCode: 201,
   data
