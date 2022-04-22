39475a84bf5c7f428f31a5354caebf20a0850f8f
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-22 15:08:53 -0300

    chore: add .env

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  port: 5432,
-  host: process.env.NODE_ENV === 'test' ? 'localhost' : 'postgres',
-  username: 'docker',
-  password: 'docker_postgres',
-  database: 'codeBurguer',
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   migrations: ['./src/infra/database/postgres/helpers/migrations/*.ts'],
   entities: ['./src/infra/database/postgres/entities/*.ts'],
   cli: { migrationsDir: './src/infra/database/postgres/helpers/migrations' }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,12 @@
 import './config/module-alias'
-import { env } from '@/main/config'
 import { PgConnection } from '@/infra/database/postgres/helpers'
 
 import 'reflect-metadata'
 
 PgConnection.getInstance().connect()
   .then(async () => {
-    const { app } = await import('@/main/config/app')
-    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+    const { app, env: { port } } = await import('@/main/config')
+
+    app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
   })
   .catch(console.error)
