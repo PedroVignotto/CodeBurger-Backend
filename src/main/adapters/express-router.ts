f93d3cdf434dd501660464c90e4c2a5227f32895
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-21 22:11:27 -0300

    feat: add statusCode 201 on ExpressRouter

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -8,7 +8,7 @@ type Adapter = (controller: Controller) => RequestHandler
 export const expressRouterAdapter: Adapter = controller => async (req, res) => {
   const { statusCode, data } = await controller.handle({ ...req.body, ...req.params })
 
-  const json = statusCode === 200 ? data : { error: data.message }
+  const json = [200, 201].includes(statusCode) ? data : { error: data.message }
 
   res.status(statusCode).json(json)
 }
