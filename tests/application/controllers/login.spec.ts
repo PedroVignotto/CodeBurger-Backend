d986ea970d64298bd3241802d9926e0ae70e9f55
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-22 07:32:56 -0300

    test: ensure LoginController build validators correctly

--- tests/application/controllers/login.spec.ts
+++ tests/application/controllers/login.spec.ts
@@ -1,15 +1,34 @@
 import { Controller, LoginController } from '@/application/controllers'
+import { EmailValidation, RequiredValidation } from '@/application/validation'
+
+import faker from 'faker'
 
 describe('LoginController', () => {
   let sut: LoginController
+
+  let email: string
+  let password: string
 
   const authentication: jest.Mock = jest.fn()
 
   beforeEach(() => {
     sut = new LoginController(authentication)
+
+    email = faker.internet.email()
+    password = faker.internet.password(8)
   })
 
   it('Should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ email, password })
+
+    expect(validators).toEqual([
+      new RequiredValidation(email, 'email'),
+      new EmailValidation(email, 'email'),
+      new RequiredValidation(password, 'password')
+    ])
   })
 })
