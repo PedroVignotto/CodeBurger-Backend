a2cd4f04363817f2b4830187efcfaf84483bd623
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-23 20:05:26 -0300

    test: ensure Authorize return accountId on success

--- tests/domain/use-cases/account/authorize.spec.ts
+++ tests/domain/use-cases/account/authorize.spec.ts
@@ -7,23 +7,32 @@ import faker from 'faker'
 describe('Authorize', () => {
   let sut: Authorize
 
+  let accountId: string
   let accessToken: string
 
-  let token = mock<TokenValidator>()
+  const token = mock<TokenValidator>()
 
   beforeAll(() => {
+    accountId = faker.datatype.uuid()
     accessToken = faker.datatype.uuid()
-    token = mock()
+
+    token.validate.mockResolvedValue(accountId)
   })
 
   beforeEach(() => {
     sut = AuthorizeUseCase(token)
   })
 
-  it('should call TokenValidator with correct token', async () => {
+  it('Should call TokenValidator with correct token', async () => {
     await sut({ accessToken })
 
     expect(token.validate).toHaveBeenCalledWith({ token: accessToken })
     expect(token.validate).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return a accountId on success', async () => {
+    const result = await sut({ accessToken })
+
+    expect(result).toBe(accountId)
   })
 })
