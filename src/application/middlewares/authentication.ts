52a70cf825f100f26489f07f940777f091ad60cc
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 10:27:56 -0300

    feat: ensure AuthenticationMiddleware implements Middleware

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -1,10 +1,11 @@
 import { forbidden, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
+import { Middleware } from '@/application/middlewares/middleware'
 import { Authorize } from '@/domain/use-cases/account'
 
 type HttpRequest = { authorization: string }
 type Model = { accountId: string } | Error
 
-export class AuthenticationMiddleware {
+export class AuthenticationMiddleware implements Middleware {
   constructor (private readonly authorize: Authorize, private readonly role?: string) {}
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
