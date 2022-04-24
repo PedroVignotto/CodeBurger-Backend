64b80e990eccb5789c19aa22e84a12bec031b297
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 20:08:26 -0300

    feat: ensure AddAddressUseCase call AddAddressRepository with correct values

--- src/domain/use-cases/address/add-address.ts
+++ src/domain/use-cases/address/add-address.ts
@@ -1,12 +1,17 @@
+import { AddAddressRepository } from '@/domain/contracts/database/repositories/address'
 import { SearchAddressByZipCode } from '@/domain/contracts/gateways'
 
-type Setup = (searchAddressByZipCode: SearchAddressByZipCode) => AddAddress
-type Input = { zipCode: string }
+type Setup = (searchAddressByZipCode: SearchAddressByZipCode, addressRepository: AddAddressRepository) => AddAddress
+type Input = { accountId: string, surname: string, zipCode: string, district: string, address: string, number: number, complement?: string }
 type Output = undefined
 export type AddAddress = (input: Input) => Promise<Output>
 
-export const AddAddressUseCase: Setup = (searchAddressByZipCode) => async input => {
-  await searchAddressByZipCode.search({ zipCode: input.zipCode })
+export const AddAddressUseCase: Setup = (searchAddressByZipCode, addressRepository) => async input => {
+  const account = await searchAddressByZipCode.search({ zipCode: input.zipCode })
+
+  if (!account) return undefined
+
+  await addressRepository.create(input)
 
   return undefined
 }
