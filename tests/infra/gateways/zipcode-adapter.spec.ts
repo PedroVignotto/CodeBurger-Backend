34feaca49165b6d48ef63fbe4a2dd435ea3c590a
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 13:55:08 -0300

    test: ensure ZipCodeApi return district and address on success

--- tests/infra/gateways/zipcode-adapter.spec.ts
+++ tests/infra/gateways/zipcode-adapter.spec.ts
@@ -7,15 +7,21 @@ describe('ZipCodeApi', () => {
   let sut: ZipCodeApi
 
   let zipcode: string
+  let district: string
+  let address: string
 
   const httpClient = mock<HttpGetClient>()
 
   beforeAll(() => {
     zipcode = faker.address.zipCode('########')
+    district = faker.random.words(2)
+    address = faker.address.streetName()
   })
 
   beforeEach(() => {
     sut = new ZipCodeApi(httpClient)
+
+    httpClient.get.mockResolvedValue({ data: { status: 200, district, address } })
   })
 
   it('Should call get with correct values', async () => {
@@ -26,8 +32,16 @@ describe('ZipCodeApi', () => {
   })
 
   it('Should return undefined if get not find the zip code', async () => {
+    httpClient.get.mockResolvedValue({ data: { status: 404 } })
+
     const result = await sut.search({ zipcode })
 
     expect(result).toBeUndefined()
+  })
+
+  it('Should return district and address on success', async () => {
+    const result = await sut.search({ zipcode })
+
+    expect(result).toEqual({ district, address })
   })
 })
