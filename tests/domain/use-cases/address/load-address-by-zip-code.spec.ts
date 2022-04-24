190eb111a0624e2453dbbad2f4242aa97b50ee2e
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 18:33:39 -0300

    test: ensure LoadAddressByZipCodeUseCase call SearchAddressByZipCode with correct zipcode

---
+++ tests/domain/use-cases/address/load-address-by-zip-code.spec.ts
@@ -0,0 +1,26 @@
+import { SearchAddressByZipCode } from '@/domain/contracts/gateways'
+import { LoadAddressByZipCode, LoadAddressByZipCodeUseCase } from '@/domain/use-cases/address'
+
+import { mock } from 'jest-mock-extended'
+import faker from 'faker'
+
+describe('LoadAddressByZipCodeUseCase', () => {
+  let sut: LoadAddressByZipCode
+
+  let zipCode: string
+
+  const searchAddressByZipCode = mock<SearchAddressByZipCode>()
+
+  beforeEach(() => {
+    sut = LoadAddressByZipCodeUseCase(searchAddressByZipCode)
+
+    zipCode = faker.address.zipCode('########')
+  })
+
+  it('Should call SearchAddressByZipCode with correct zipcode', async () => {
+    await sut({ zipCode })
+
+    expect(searchAddressByZipCode.search).toHaveBeenCalledWith({ zipCode })
+    expect(searchAddressByZipCode.search).toHaveBeenCalledTimes(1)
+  })
+})
