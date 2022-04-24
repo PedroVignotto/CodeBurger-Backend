b7e9da7f680b82d9f68ec284efddab815f10f4fb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 18:54:32 -0300

    test: ensure LoadAddressByZipCodeController call loadAddressByZipCode with correct values

--- tests/application/controllers/address/load-address-by-zipcode.spec.ts
+++ tests/application/controllers/address/load-address-by-zipcode.spec.ts
@@ -1,16 +1,29 @@
 import { Controller } from '@/application/controllers'
 import { LoadAddressByZipCodeController } from '@/application/controllers/address'
 
+import faker from 'faker'
+
 describe('LoadAddressByZipCodeController', () => {
   let sut: LoadAddressByZipCodeController
+
+  let zipCode: string
 
   const loadAddressByZipCode: jest.Mock = jest.fn()
 
   beforeEach(() => {
     sut = new LoadAddressByZipCodeController(loadAddressByZipCode)
+
+    zipCode = faker.address.zipCode('########')
   })
 
   it('Should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should call loadAddressByZipCode with correct values', async () => {
+    await sut.handle({ zipCode })
+
+    expect(loadAddressByZipCode).toHaveBeenCalledWith({ zipCode })
+    expect(loadAddressByZipCode).toHaveBeenCalledTimes(1)
   })
 })
