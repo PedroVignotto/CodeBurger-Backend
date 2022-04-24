ddc91e60356a7df0664862f1c36a0fd9b49f44a8
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-23 23:51:55 -0300

    feat: ensure Authorize return undefined if CheckAccountRole return false

--- src/domain/use-cases/account/authorize.ts
+++ src/domain/use-cases/account/authorize.ts
@@ -3,13 +3,15 @@ import { TokenValidator } from '@/domain/contracts/gateways'
 
 type Setup = (token: TokenValidator, accountRepository: CheckAccountRole) => Authorize
 type Input = { accessToken: string, role?: string }
-type Output = string
+type Output = string | undefined
 export type Authorize = (input: Input) => Promise<Output>
 
 export const AuthorizeUseCase: Setup = (token, accountRepository) => async ({ accessToken, role }) => {
   const accountId = await token.validate({ token: accessToken })
 
-  await accountRepository.checkRole({ accountId, role })
+  const account = await accountRepository.checkRole({ accountId, role })
+
+  if (!account) return undefined
 
   return accountId
 }
