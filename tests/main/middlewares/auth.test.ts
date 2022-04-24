71f51f0cdfed10e2c08349ec32eb3bea0bbac6b1
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-24 10:08:25 -0300

    test: ensure AuthMiddleware return 200 if authorization header is valid

--- tests/main/middlewares/auth.test.ts
+++ tests/main/middlewares/auth.test.ts
@@ -1,16 +1,66 @@
-import { UnauthorizedError } from '@/application/errors'
-import { app } from '@/main/config/app'
+import { makeFakeDatabase } from '@/tests/infra/database/postgres/mocks'
+import { app, env } from '@/main/config'
 import { auth } from '@/main/middlewares'
+import { UnauthorizedError } from '@/application/errors'
+import { Account } from '@/infra/database/postgres/entities'
+import { PgConnection } from '@/infra/database/postgres/helpers'
 
+import { IBackup, IMemoryDb } from 'pg-mem'
+import { sign } from 'jsonwebtoken'
+import { Repository } from 'typeorm'
 import request from 'supertest'
+import faker from 'faker'
 
 describe('Auth Middleware', () => {
-  it('Should return 401 if Authorization header was not provided', async () => {
+  let id: string
+  let name: string
+  let email: string
+  let password: string
+
+  let connection: PgConnection
+  let database: IMemoryDb
+  let backup: IBackup
+  let repository: Repository<Account>
+
+  beforeAll(async () => {
+    connection = PgConnection.getInstance()
+    database = await makeFakeDatabase([Account])
+    backup = database.backup()
+    repository = connection.getRepository(Account)
+  })
+
+  beforeEach(() => {
+    backup.restore()
+
+    id = faker.datatype.uuid()
+    name = faker.name.findName()
+    email = faker.internet.email()
+    password = faker.internet.password()
+  })
+
+  afterAll(async () => {
+    await connection.disconnect()
+  })
+
+  it('Should return 401 if authorization header was not provided', async () => {
     app.get('/fake_route', auth)
 
     const { status, body } = await request(app).get('/fake_route')
 
     expect(status).toBe(401)
     expect(body.error).toBe(new UnauthorizedError().message)
+  })
+
+  it('Should return 200 if authorization header is valid', async () => {
+    const account = await repository.save({ id, name, email, password })
+    const token = sign({ key: account.id }, env.secret)
+    app.get('/fake_route', auth, (req, res) => { res.json(req.locals) })
+
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization: `Bearer: ${token}` })
+
+    expect(status).toBe(200)
+    expect(body).toEqual({ accountId: id })
   })
 })
