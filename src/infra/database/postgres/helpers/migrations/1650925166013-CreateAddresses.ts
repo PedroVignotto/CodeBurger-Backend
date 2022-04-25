925f44ababaa35e1b790ee8a1336ef7617072187
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-25 20:29:22 -0300

    feat: add migration to addresses table

---
+++ src/infra/database/postgres/helpers/migrations/1650925166013-CreateAddresses.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateAddresses1650925166013 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'addresses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'accountId',
+            type: 'uuid'
+          },
+          {
+            name: 'surname',
+            type: 'varchar'
+          },
+          {
+            name: 'zipCode',
+            type: 'varchar'
+          },
+          {
+            name: 'district',
+            type: 'varchar'
+          },
+          {
+            name: 'address',
+            type: 'varchar'
+          },
+          {
+            name: 'number',
+            type: 'numeric'
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FKAccountAddress',
+            referencedTableName: 'accounts',
+            referencedColumnNames: ['id'],
+            columnNames: ['accountId'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('addresses')
+  }
+}
