ffc10c2556f2b0a10218839e0d02342e20704267
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-25 13:03:50 -0300

    feat: add AddAddressController route

--- src/main/routes/address.ts
+++ src/main/routes/address.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeLoadAddressByZipCodeController } from '@/main/factories/application/controllers/address'
+import { makeAddAddressController, makeLoadAddressByZipCodeController } from '@/main/factories/application/controllers/address'
 import { auth } from '@/main/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/address/:zipCode', auth, adapt(makeLoadAddressByZipCodeController()))
+  router.post('/address', auth, adapt(makeAddAddressController()))
 }
