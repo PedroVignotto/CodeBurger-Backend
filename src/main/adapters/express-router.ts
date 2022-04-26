f34adaed7d23b132e9a428c18a840707801de429
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 20:11:45 -0300

    feat: add statusCode 204 on ExpressRouter

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -7,7 +7,7 @@ type Adapter = (controller: Controller) => RequestHandler
 export const expressRouterAdapter: Adapter = controller => async (req, res) => {
   const { statusCode, data } = await controller.handle({ ...req.body, ...req.params, ...req.locals })
 
-  const json = [200, 201].includes(statusCode) ? data : { error: data.message }
+  const json = [200, 201, 204].includes(statusCode) ? data : { error: data.message }
 
   res.status(statusCode).json(json)
 }
