b84d1d6ae01942c03c273a036040c9280e36fffa
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 19:55:31 -0300

    feat: add UpdateAddressController route

--- src/main/routes/address.ts
+++ src/main/routes/address.ts
@@ -1,5 +1,5 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeAddAddressController, makeListAddressesController, makeLoadAddressByZipCodeController } from '@/main/factories/application/controllers/address'
+import { makeAddAddressController, makeListAddressesController, makeLoadAddressByZipCodeController, makeUpdateAddressController } from '@/main/factories/application/controllers/address'
 import { auth } from '@/main/middlewares'
 
 import { Router } from 'express'
@@ -8,4 +8,5 @@ export default (router: Router): void => {
   router.get('/address/:zipCode', auth, adapt(makeLoadAddressByZipCodeController()))
   router.post('/address', auth, adapt(makeAddAddressController()))
   router.get('/addresses', auth, adapt(makeListAddressesController()))
+  router.put('/address/:id', auth, adapt(makeUpdateAddressController()))
 }
