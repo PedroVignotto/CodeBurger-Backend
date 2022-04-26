0346d169578eea7eb724a67e674a0be3f0d17ac0
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-25 21:34:37 -0300

    feat: ensure ListAddressRepository return all addresses

--- src/infra/database/postgres/repositories/address.ts
+++ src/infra/database/postgres/repositories/address.ts
@@ -1,9 +1,9 @@
 import { Address } from '@/infra/database/postgres/entities'
 import { PgRepository } from '@/infra/database/postgres/repositories'
 import { UUIDGenerator } from '@/domain/contracts/gateways'
-import { AddAddressRepository } from '@/domain/contracts/database/repositories/address'
+import { AddAddressRepository, ListAddressRepository } from '@/domain/contracts/database/repositories/address'
 
-export class AddressRepository extends PgRepository implements AddAddressRepository {
+export class AddressRepository extends PgRepository implements AddAddressRepository, ListAddressRepository {
   constructor (private readonly uuid: UUIDGenerator) { super() }
 
   async create (input: AddAddressRepository.Input): Promise<AddAddressRepository.Output> {
@@ -12,5 +12,13 @@ export class AddressRepository extends PgRepository implements AddAddressReposit
     await repository.save({ id: this.uuid.generate(), ...input })
 
     return true
+  }
+
+  async list ({ accountId }: ListAddressRepository.Input): Promise<ListAddressRepository.Output> {
+    const repository = this.getRepository(Address)
+
+    const addresses = await repository.find({ accountId })
+
+    return addresses
   }
 }
