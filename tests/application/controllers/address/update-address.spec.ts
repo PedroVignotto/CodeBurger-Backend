0e11e30214003b2f9d169cf4037f0533fbb64017
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 19:44:33 -0300

    test: ensure UpdateAddressController call updateAddress with correct values

---
+++ tests/application/controllers/address/update-address.spec.ts
@@ -0,0 +1,26 @@
+import { addressParams } from '@/tests/mocks'
+import { Controller } from '@/application/controllers'
+import { UpdateAddressController } from '@/application/controllers/address'
+
+describe('UpdateAddressController', () => {
+  let sut: UpdateAddressController
+
+  const { id, surname, number, complement } = addressParams
+
+  const updateAddress: jest.Mock = jest.fn()
+
+  beforeEach(() => {
+    sut = new UpdateAddressController(updateAddress)
+  })
+
+  it('Should extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should call updateAddress with correct values', async () => {
+    await sut.handle({ id, surname, number, complement })
+
+    expect(updateAddress).toHaveBeenCalledWith({ id, surname, number, complement })
+    expect(updateAddress).toHaveBeenCalledTimes(1)
+  })
+})
