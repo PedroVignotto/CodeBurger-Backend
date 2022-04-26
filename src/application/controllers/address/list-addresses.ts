2b8d53f42790a6071f3e1fc1c17beb9caf1eb913
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-25 21:55:47 -0300

    feat: ensure ListAddressController return ok if valid data is provided

--- src/application/controllers/address/list-addresses.ts
+++ src/application/controllers/address/list-addresses.ts
@@ -3,14 +3,14 @@ import { HttpResponse, ok } from '@/application/helpers'
 import { ListAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { accountId: string }
-type Model = undefined
+type Model = Array<{ id: string, surname: string, zipCode: string, district: string, address: string, number: number, complement?: string }>
 
 export class ListAddressController extends Controller {
   constructor (private readonly listAddress: ListAddress) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse<Model>> {
-    await this.listAddress({ accountId })
+    const addresses = await this.listAddress({ accountId })
 
-    return ok(undefined)
+    return ok(addresses)
   }
 }
