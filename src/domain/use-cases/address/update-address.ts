9e25cb1d8e7a5ce9378cdea3ccf572b9fe542670
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 23:58:11 -0300

    feat: ensure UpdateAddressUseCase return false if CheckAddressByIdRepository return false

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -2,9 +2,11 @@ import { CheckAddressByIdRepository } from '@/domain/contracts/database/reposito
 
 type Setup = (addressRepository: CheckAddressByIdRepository) => UpdateAddress
 type Input = { id: string, surname?: string, number?: number, complement?: string }
-type Output = void
+type Output = boolean
 export type UpdateAddress = (input: Input) => Promise<Output>
 
 export const updateAddressUseCase: Setup = addressRepository => async ({ id }) => {
   await addressRepository.checkById({ id })
+
+  return false
 }
