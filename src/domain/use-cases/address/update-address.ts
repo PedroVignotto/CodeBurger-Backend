42d8dadb0b114078683e1089c6b354bf5d96e220
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 23:56:58 -0300

    feat: ensure UpdateAddressUseCase call CheckAddressByIdRepository with correct id

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -1,3 +1,10 @@
+import { CheckAddressByIdRepository } from '@/domain/contracts/database/repositories/address'
+
+type Setup = (addressRepository: CheckAddressByIdRepository) => UpdateAddress
 type Input = { id: string, surname?: string, number?: number, complement?: string }
 type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
+
+export const updateAddressUseCase: Setup = addressRepository => async ({ id }) => {
+  await addressRepository.checkById({ id })
+}
