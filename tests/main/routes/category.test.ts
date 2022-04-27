609d5bb4b908d57e7a2cdc869e8d33a5d65d5060
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 18:22:41 -0300

    feat: ensure AddCategoryController return 400 if name already exists

--- tests/main/routes/category.test.ts
+++ tests/main/routes/category.test.ts
@@ -1,33 +1,36 @@
 import { accountParams, categoryParams } from '@/tests/mocks'
 import { makeFakeDatabase } from '@/tests/infra/database/postgres/mocks'
 import { app } from '@/main/config/app'
-import { RequiredFieldError } from '@/application/errors'
+import { FieldInUseError, RequiredFieldError } from '@/application/errors'
 import { Account, Category } from '@/infra/database/postgres/entities'
 import { PgConnection } from '@/infra/database/postgres/helpers'
 
 import { IBackup, IMemoryDb } from 'pg-mem'
 import request from 'supertest'
+import { Repository } from 'typeorm'
 
 describe('Category routes', () => {
-  const { name, email, password, passwordConfirmation } = accountParams
-  const { name: categoryName } = categoryParams
+  const { name: accountName, email, password, passwordConfirmation } = accountParams
+  const { id, name } = categoryParams
 
   let token: string
 
   let connection: PgConnection
   let database: IMemoryDb
   let backup: IBackup
+  let repository: Repository<Category>
 
   beforeAll(async () => {
     connection = PgConnection.getInstance()
     database = await makeFakeDatabase([Account, Category])
     backup = database.backup()
+    repository = connection.getRepository(Category)
   })
 
   beforeEach(async () => {
     backup.restore()
 
-    const { body } = await request(app).post('/api/signup').send({ name, email, password, passwordConfirmation })
+    const { body } = await request(app).post('/api/signup').send({ name: accountName, email, password, passwordConfirmation })
 
     token = body.accessToken
   })
@@ -39,7 +42,7 @@ describe('Category routes', () => {
   it('Should return 201 on success', async () => {
     const { status } = await request(app)
       .post('/api/category')
-      .send({ name: categoryName })
+      .send({ name })
       .set({ authorization: `Bearer: ${token}` })
 
     expect(status).toBe(201)
@@ -53,5 +56,16 @@ describe('Category routes', () => {
 
     expect(status).toBe(400)
     expect(error).toBe(new RequiredFieldError('name').message)
+  })
+
+  it('Should return 400 if name already exists', async () => {
+    await repository.save({ id, name })
+    const { status, body: { error } } = await request(app)
+      .post('/api/category')
+      .send({ name })
+      .set({ authorization: `Bearer: ${token}` })
+
+    expect(status).toBe(400)
+    expect(error).toBe(new FieldInUseError('name').message)
   })
 })
