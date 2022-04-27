118a1d2d1461cc5539d6c24ed7edec68db638360
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 00:07:14 -0300

    feat: ensure UpdateAddressController return badRequest if updateAddress return false

--- src/application/controllers/address/update-address.ts
+++ src/application/controllers/address/update-address.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@/application/controllers/controller'
-import { HttpResponse, noContent } from '@/application/helpers'
+import { InvalidFieldError } from '@/application/errors'
+import { badRequest, HttpResponse, noContent } from '@/application/helpers'
 import { UpdateAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { id: string, surname?: string, number?: number, complement?: string }
@@ -8,7 +9,9 @@ export class UpdateAddressController extends Controller {
   constructor (private readonly updateAddress: UpdateAddress) { super() }
 
   async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.updateAddress(httpRequest)
+    const address = await this.updateAddress(httpRequest)
+
+    if (!address) return badRequest(new InvalidFieldError('id'))
 
     return noContent()
   }
