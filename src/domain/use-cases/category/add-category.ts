8910eef5348ef267004c602504e04215fabd62eb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 16:43:13 -0300

    feat: ensure AddCategoryUseCase call CheckCategoryByNameRepository with correct name

---
+++ src/domain/use-cases/category/add-category.ts
@@ -0,0 +1,10 @@
+import { CheckCategoryByNameRepository } from '@/domain/contracts/database/repositories/category'
+
+type Setup = (categoryRepository: CheckCategoryByNameRepository) => AddCategory
+type Input = { name: string }
+type Output = void
+export type AddCategory = (input: Input) => Promise<Output>
+
+export const addCategoryUseCase: Setup = categoryRepository => async ({ name }) => {
+  await categoryRepository.checkByName({ name })
+}
