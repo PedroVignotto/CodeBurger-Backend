92663d20483b1459f940f94daaec862c9dac0f23
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 02:57:34 -0300

    fix: add bind on makeLoadAddressByZipCodeUseCase

--- src/main/factories/domain/use-cases/address/load-address-by-zipcode.ts
+++ src/main/factories/domain/use-cases/address/load-address-by-zipcode.ts
@@ -2,5 +2,5 @@ import { makeSearchAddressByZipCode } from '@/main/factories/infra/gateways'
 import { LoadAddressByZipCode } from '@/domain/use-cases/address'
 
 export const makeLoadAddressByZipCodeUseCase = (): LoadAddressByZipCode => {
-  return makeSearchAddressByZipCode().search
+  return makeSearchAddressByZipCode().search.bind(makeSearchAddressByZipCode())
 }
