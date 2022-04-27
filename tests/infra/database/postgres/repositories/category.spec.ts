a0f90d3feabda4339072f2eaa60979da3f609bef
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 17:18:07 -0300

    test: ensure CategoryRepository return true if category already exists

--- tests/infra/database/postgres/repositories/category.spec.ts
+++ tests/infra/database/postgres/repositories/category.spec.ts
@@ -7,21 +7,25 @@ import { UUIDGenerator } from '@/domain/contracts/gateways'
 
 import { IBackup, IMemoryDb } from 'pg-mem'
 import { mock } from 'jest-mock-extended'
+import { Repository } from 'typeorm'
 
 describe('CategoryRepository', () => {
   let sut: CategoryRepository
 
   const { id, name } = categoryParams
 
+  let connection: PgConnection
   let database: IMemoryDb
   let backup: IBackup
+  let repository: Repository<Category>
 
   const uuid = mock<UUIDGenerator>()
 
   beforeAll(async () => {
-    PgConnection.getInstance()
+    connection = PgConnection.getInstance()
     database = await makeFakeDatabase([Category])
     backup = database.backup()
+    repository = connection.getRepository(Category)
 
     uuid.generate.mockReturnValue(id)
   })
@@ -40,5 +44,13 @@ describe('CategoryRepository', () => {
     const categoryExists = await sut.checkByName({ name })
 
     expect(categoryExists).toBe(false)
+  })
+
+  it('Should return true if category already exists', async () => {
+    await repository.save({ id, name })
+
+    const categoryExists = await sut.checkByName({ name })
+
+    expect(categoryExists).toBe(true)
   })
 })
