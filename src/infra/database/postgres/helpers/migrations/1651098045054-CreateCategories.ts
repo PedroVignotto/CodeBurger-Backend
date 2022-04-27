144cbf85c09e6b32b6f360ae4a44d1c6b85eca17
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 19:24:59 -0300

    feat: add Categories migration

---
+++ src/infra/database/postgres/helpers/migrations/1651098045054-CreateCategories.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateCategories1651098045054 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories')
+  }
+}
