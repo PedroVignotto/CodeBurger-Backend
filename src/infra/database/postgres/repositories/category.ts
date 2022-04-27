9f778d3b122a2ea2ed25f46c517dd36d4968e02b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 17:21:08 -0300

    feat: ensure CategoryRepository create a category on success

--- src/infra/database/postgres/repositories/category.ts
+++ src/infra/database/postgres/repositories/category.ts
@@ -1,9 +1,9 @@
 import { Category } from '@/infra/database/postgres/entities'
 import { PgRepository } from '@/infra/database/postgres/repositories'
 import { UUIDGenerator } from '@/domain/contracts/gateways'
-import { CheckCategoryByNameRepository } from '@/domain/contracts/database/repositories/category'
+import { AddCategoryRepository, CheckCategoryByNameRepository } from '@/domain/contracts/database/repositories/category'
 
-export class CategoryRepository extends PgRepository implements CheckCategoryByNameRepository {
+export class CategoryRepository extends PgRepository implements CheckCategoryByNameRepository, AddCategoryRepository {
   constructor (private readonly uuid: UUIDGenerator) { super() }
 
   async checkByName ({ name }: CheckCategoryByNameRepository.Input): Promise<CheckCategoryByNameRepository.Output> {
@@ -12,5 +12,11 @@ export class CategoryRepository extends PgRepository implements CheckCategoryByN
     const categoryExists = await repository.findOne({ name })
 
     return !!categoryExists
+  }
+
+  async create ({ name }: AddCategoryRepository.Input): Promise<AddCategoryRepository.Output> {
+    const repository = this.getRepository(Category)
+
+    await repository.save({ id: this.uuid.generate(), name })
   }
 }
