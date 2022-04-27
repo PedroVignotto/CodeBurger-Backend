5ce88c8fd267350fcf8079f6fd76de452ad5aedc
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-26 21:00:32 -0300

    feat: ensure DeleteAddressUseCase call DeleteAddressRepository with correct id

--- src/domain/use-cases/address/delete-address.ts
+++ src/domain/use-cases/address/delete-address.ts
@@ -1,12 +1,16 @@
-import { CheckAddressByIdRepository } from '@/domain/contracts/database/repositories/address'
+import { CheckAddressByIdRepository, DeleteAddressRepository } from '@/domain/contracts/database/repositories/address'
 
-type Setup = (addressRepository: CheckAddressByIdRepository) => DeleteAddress
+type Setup = (addressRepository: CheckAddressByIdRepository & DeleteAddressRepository) => DeleteAddress
 type Input = { id: string }
 type Output = boolean
 export type DeleteAddress = (input: Input) => Promise<Output>
 
 export const deleteAddressUseCase: Setup = addressRepository => async ({ id }) => {
-  await addressRepository.checkById({ id })
+  const address = await addressRepository.checkById({ id })
 
-  return false
+  if (!address) return false
+
+  await addressRepository.delete({ id })
+
+  return true
 }
