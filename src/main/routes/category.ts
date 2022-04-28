ab62beccb7048e9d806c4d3db19732c6571580cb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 22:01:53 -0300

    feat: add DeleteCategoryController route

--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,10 +1,11 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeAddCategoryController, makeListCategoriesController } from '@/main/factories/application/controllers/category'
+import { makeAddCategoryController, makeDeleteCategoryController, makeListCategoriesController } from '@/main/factories/application/controllers/category'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/category', auth, adapt(makeAddCategoryController()))
   router.get('/categories', auth, adapt(makeListCategoriesController()))
+  router.delete('/category/:id', auth, adapt(makeDeleteCategoryController()))
 }
