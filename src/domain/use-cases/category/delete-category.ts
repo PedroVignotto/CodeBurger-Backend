c5b1d8d0117cf06f75a2fbee0eb55cd134ba0113
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 21:36:57 -0300

    feat: ensure DeleteCategoryUseCase call CheckCategoryByIdRepository with correct id

---
+++ src/domain/use-cases/category/delete-category.ts
@@ -0,0 +1,10 @@
+import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
+
+type Setup = (categoryRepository: CheckCategoryByIdRepository) => DeleteCategory
+type Input = { id: string }
+type Output = void
+export type DeleteCategory = (input: Input) => Promise<Output>
+
+export const deleteCategoryUseCase: Setup = categoryRepository => async ({ id }) => {
+  await categoryRepository.checkById({ id })
+}
