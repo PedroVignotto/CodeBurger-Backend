3548eb1f6241ef05e8a3763217b80e793cbdc790
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 21:10:28 -0300

    feat: ensure ListCategoriesController call listCategories with correct values

---
+++ src/application/controllers/category/list-categories.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@/application/controllers/controller'
+import { HttpResponse, ok } from '@/application/helpers'
+import { ListCategories } from '@/domain/use-cases/category'
+
+type Model = Array<{ id: string, name: string }> | Error
+
+export class ListCategoriesController extends Controller {
+  constructor (private readonly listCategories: ListCategories) { super() }
+
+  async perform (): Promise<HttpResponse<Model>> {
+    const categories = await this.listCategories()
+
+    return ok(categories)
+  }
+}
