ccab3bbcbf7dbddacbcbccdd5a914cdb7793f2c1
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-27 21:56:57 -0300

    feat: ensure DeleteCategoryController return badRequest if deleteAddress return false

--- src/application/controllers/category/delete-category.ts
+++ src/application/controllers/category/delete-category.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@/application/controllers/controller'
-import { HttpResponse, ok } from '@/application/helpers'
+import { InvalidFieldError } from '@/application/errors'
+import { badRequest, HttpResponse, ok } from '@/application/helpers'
 import { DeleteCategory } from '@/domain/use-cases/category'
 
 type HttpRequest = { id: string }
@@ -9,7 +10,9 @@ export class DeleteCategoryController extends Controller {
   constructor (private readonly deleteCategory: DeleteCategory) { super() }
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse<Model>> {
-    await this.deleteCategory({ id })
+    const category = await this.deleteCategory({ id })
+
+    if (!category) return badRequest(new InvalidFieldError('id'))
 
     return ok(undefined)
   }
