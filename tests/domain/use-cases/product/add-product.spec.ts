2e52d2c905ba72e279d1ae66078b6552085541f0
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 18:01:16 -0300

    test: ensure AddProductUseCase return FieldInUseError if CheckProductByNameRepository return true

--- tests/domain/use-cases/product/add-product.spec.ts
+++ tests/domain/use-cases/product/add-product.spec.ts
@@ -1,6 +1,7 @@
 import { categoryParams } from '@/tests/mocks'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { AddProduct, addProductUseCase } from '@/domain/use-cases/product'
+import { FieldInUseError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -20,5 +21,13 @@ describe('AddProductUseCase', () => {
 
     expect(productRepository.checkByName).toHaveBeenCalledWith({ name })
     expect(productRepository.checkByName).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return FieldInUseError if CheckProductByNameRepository return true', async () => {
+    productRepository.checkByName.mockResolvedValueOnce(true)
+
+    const result = await sut({ name })
+
+    expect(result).toEqual(new FieldInUseError('name'))
   })
 })
