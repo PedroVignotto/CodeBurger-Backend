692d9b1ec7ae0ea6e3c224b1cba65ab29437b269
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 20:03:53 -0300

    test: ensure AddProductUseCase call AddProductRepository with correct values

--- tests/domain/use-cases/product/add-product.spec.ts
+++ tests/domain/use-cases/product/add-product.spec.ts
@@ -1,5 +1,5 @@
 import { categoryParams, productParams } from '@/tests/mocks'
-import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
+import { AddProductRepository, CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { AddProduct, addProductUseCase } from '@/domain/use-cases/product'
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
@@ -11,9 +11,9 @@ describe('AddProductUseCase', () => {
   let sut: AddProduct
 
   const { id: categoryId } = categoryParams
-  const { name, key, file } = productParams
+  const { name, description, price, key, picture, file } = productParams
 
-  const productRepository = mock<CheckProductByNameRepository>()
+  const productRepository = mock<CheckProductByNameRepository & AddProductRepository>()
   const categoryRepository = mock<CheckCategoryByIdRepository>()
   const uuid = mock<UUIDGenerator>()
   const fileStorage = mock<UploadFile>()
@@ -22,6 +22,7 @@ describe('AddProductUseCase', () => {
     productRepository.checkByName.mockResolvedValue(false)
     categoryRepository.checkById.mockResolvedValue(true)
     uuid.generate.mockReturnValue(key)
+    fileStorage.upload.mockResolvedValue(picture)
   })
 
   beforeEach(() => {
@@ -29,7 +30,7 @@ describe('AddProductUseCase', () => {
   })
 
   it('Should call CheckProductByNameRepository with correct name', async () => {
-    await sut({ categoryId, name, file })
+    await sut({ categoryId, name, description, price, file })
 
     expect(productRepository.checkByName).toHaveBeenCalledWith({ name })
     expect(productRepository.checkByName).toHaveBeenCalledTimes(1)
@@ -38,13 +39,13 @@ describe('AddProductUseCase', () => {
   it('Should return FieldInUseError if CheckProductByNameRepository return true', async () => {
     productRepository.checkByName.mockResolvedValueOnce(true)
 
-    const result = await sut({ categoryId, name, file })
+    const result = await sut({ categoryId, name, description, price, file })
 
     expect(result).toEqual(new FieldInUseError('name'))
   })
 
   it('Should call CheckCategoryByIdRepository with correct id', async () => {
-    await sut({ categoryId, name, file })
+    await sut({ categoryId, name, description, price, file })
 
     expect(categoryRepository.checkById).toHaveBeenCalledWith({ id: categoryId })
     expect(categoryRepository.checkById).toHaveBeenCalledTimes(1)
@@ -53,22 +54,29 @@ describe('AddProductUseCase', () => {
   it('Should return NonExistentFieldError if CheckCategoryByIdRepository return false', async () => {
     categoryRepository.checkById.mockResolvedValueOnce(false)
 
-    const result = await sut({ categoryId, name, file })
+    const result = await sut({ categoryId, name, description, price, file })
 
     expect(result).toEqual(new NonExistentFieldError('categoryId'))
   })
 
   it('Should call UUIDGenerator if file is provided', async () => {
-    await sut({ categoryId, name, file })
+    await sut({ categoryId, name, description, price, file })
 
     expect(uuid.generate).toHaveBeenCalledWith()
     expect(uuid.generate).toHaveBeenCalledTimes(1)
   })
 
   it('Should call UploadFile with correct values', async () => {
-    await sut({ categoryId, name, file })
+    await sut({ categoryId, name, description, price, file })
 
     expect(fileStorage.upload).toHaveBeenCalledWith({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call AddProductRepository with correct values', async () => {
+    await sut({ categoryId, name, description, price, file })
+
+    expect(productRepository.create).toHaveBeenCalledWith({ categoryId, name, description, price, picture })
+    expect(productRepository.create).toHaveBeenCalledTimes(1)
   })
 })
--- tests/mocks/product-params.ts
+++ tests/mocks/product-params.ts
@@ -5,7 +5,10 @@ const mimeTypes = ['image/png', 'image/jpeg']
 export const productParams = {
   accountId: faker.datatype.uuid(),
   name: faker.name.findName(),
+  description: faker.random.words(2),
+  price: faker.datatype.float({ max: 100 }),
   key: faker.datatype.uuid(),
+  picture: faker.internet.url(),
   file: {
     buffer: Buffer.from(faker.random.word()),
     mimeType: mimeTypes[faker.datatype.number(mimeTypes.length - 1)]
