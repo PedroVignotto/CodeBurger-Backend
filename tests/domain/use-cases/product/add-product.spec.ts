651c43645d854aeeaf34026f394c5f4afbc70cef
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 18:18:21 -0300

    test: ensure AddProductUseCase return NonExistentField if CheckCategoryByIdRepository return false

--- tests/domain/use-cases/product/add-product.spec.ts
+++ tests/domain/use-cases/product/add-product.spec.ts
@@ -2,7 +2,7 @@ import { categoryParams, productParams } from '@/tests/mocks'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { AddProduct, addProductUseCase } from '@/domain/use-cases/product'
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
-import { FieldInUseError } from '@/domain/errors'
+import { FieldInUseError, NonExistentFieldError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -39,5 +39,13 @@ describe('AddProductUseCase', () => {
 
     expect(categoryRepository.checkById).toHaveBeenCalledWith({ id: categoryId })
     expect(categoryRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return NonExistentFieldError if CheckCategoryByIdRepository return false', async () => {
+    categoryRepository.checkById.mockResolvedValueOnce(false)
+
+    const result = await sut({ categoryId, name })
+
+    expect(result).toEqual(new NonExistentFieldError('categoryId'))
   })
 })
