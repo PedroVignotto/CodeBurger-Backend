c0b272783ca3adb64538a88534e442d129af4ac3
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 18:00:53 -0300

    feat: ensure AddProductUseCase return FieldInUseError if CheckProductByNameRepository return true

--- src/domain/use-cases/product/add-product.ts
+++ src/domain/use-cases/product/add-product.ts
@@ -1,10 +1,13 @@
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
+import { FieldInUseError } from '@/domain/errors'
 
 type Setup = (productRepository: CheckProductByNameRepository) => AddProduct
 type Input = { name: string }
-type Output = void
+type Output = Error
 export type AddProduct = (input: Input) => Promise<Output>
 
 export const addProductUseCase: Setup = productRepository => async ({ name }) => {
   await productRepository.checkByName({ name })
+
+  return new FieldInUseError('name')
 }
