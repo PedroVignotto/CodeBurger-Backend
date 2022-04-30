e06c9fc531b99bb5a007ad9715a2001f5387a029
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 19:36:58 -0300

    feat: ensure AddProductUseCase call UploadFile with correct values

--- src/domain/use-cases/product/add-product.ts
+++ src/domain/use-cases/product/add-product.ts
@@ -1,14 +1,19 @@
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
-import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (productRepository: CheckProductByNameRepository, categoryRepository: CheckCategoryByIdRepository, uuid: UUIDGenerator) => AddProduct
+type Setup = (
+  productRepository: CheckProductByNameRepository,
+  categoryRepository: CheckCategoryByIdRepository,
+  uuid: UUIDGenerator,
+  fileStorage: UploadFile
+) => AddProduct
 type Input = { categoryId: string, name: string, file?: { buffer: Buffer, mimeType: string } }
 type Output = undefined | Error
 export type AddProduct = (input: Input) => Promise<Output>
 
-export const addProductUseCase: Setup = (productRepository, categoryRepository, uuid) => async ({ categoryId, name, file }) => {
+export const addProductUseCase: Setup = (productRepository, categoryRepository, uuid, fileStorage) => async ({ categoryId, name, file }) => {
   const productExists = await productRepository.checkByName({ name })
 
   if (productExists) return new FieldInUseError('name')
@@ -17,5 +22,7 @@ export const addProductUseCase: Setup = (productRepository, categoryRepository, 
 
   if (!categoryExists) return new NonExistentFieldError('categoryId')
 
-  if (file) uuid.generate()
+  const key = uuid.generate()
+
+  if (file) await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
 }
