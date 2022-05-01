d2a433d4db2f2e71502834ccaf480a34ca5e391e
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 05:31:08 -0300

    feat: add s3 on .env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,9 @@
 export const env = {
   port: process.env.PORT ?? 3333,
-  secret: process.env.SECRET ?? '585e6a367a7579ea0d122f6e462f8f91'
+  secret: process.env.SECRET ?? '585e6a367a7579ea0d122f6e462f8f91',
+  s3: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY ?? '',
+    bucket: process.env.BUCKET ?? ''
+  }
 }
