cfcc4518dc2295e1582dee1b1e76e9a2fa51e04b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 04:20:14 -0300

    test: ensure AddProductController return badRequest if addProduct return Error

--- tests/application/controllers/product/add-product.spec.ts
+++ tests/application/controllers/product/add-product.spec.ts
@@ -2,6 +2,7 @@ import { categoryParams, productParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 import { AllowedMimeTypesValidation, MaxFileSizeValidation, RequiredValidation } from '@/application/validation'
 import { AddProductController } from '@/application/controllers/product'
+import { FieldInUseError } from '@/domain/errors'
 
 describe('AddProductController', () => {
   let sut: AddProductController
@@ -37,5 +38,14 @@ describe('AddProductController', () => {
 
     expect(addProduct).toHaveBeenCalledWith({ categoryId, name, description, price, file })
     expect(addProduct).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return badRequest if addProduct return Error', async () => {
+    addProduct.mockResolvedValueOnce(new FieldInUseError('name'))
+
+    const { statusCode, data } = await sut.handle({ categoryId, name, description, price, file })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new FieldInUseError('name'))
   })
 })
