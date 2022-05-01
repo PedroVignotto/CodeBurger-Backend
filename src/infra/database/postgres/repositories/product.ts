c4f9ecc622e50db627b9479fa09dc5bf869ace5b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 23:44:37 -0300

    feat: ensure ProductRepository create a product on success

--- src/infra/database/postgres/repositories/product.ts
+++ src/infra/database/postgres/repositories/product.ts
@@ -1,11 +1,20 @@
 import { Product } from '@/infra/database/postgres/entities'
 import { PgRepository } from '@/infra/database/postgres/repositories'
-import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { AddProductRepository, CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 
-export class ProductRepository extends PgRepository implements CheckProductByNameRepository {
+export class ProductRepository extends PgRepository implements CheckProductByNameRepository, AddProductRepository {
+  constructor (private readonly uuid: UUIDGenerator) { super() }
+
   async checkByName ({ name }: CheckProductByNameRepository.Input): Promise<CheckProductByNameRepository.Output> {
     const repository = this.getRepository(Product)
 
     return !!await repository.findOne({ name })
+  }
+
+  async create ({ categoryId, name, description, price, picture }: AddProductRepository.Input): Promise<AddProductRepository.Output> {
+    const repository = this.getRepository(Product)
+
+    return await repository.save({ id: this.uuid.generate(), categoryId, name, description, price, picture })
   }
 }
