0922552f90796803f0017e972a903651e6267e97
PedroVignotto <pedro.vignotto@hotmail.com>
2022-04-30 23:47:01 -0300

    test: ensure ProductRepository create a product on success

--- tests/infra/database/postgres/repositories/product.spec.ts
+++ tests/infra/database/postgres/repositories/product.spec.ts
@@ -1,50 +1,71 @@
-import { productParams } from '@/tests/mocks'
+import { categoryParams, productParams } from '@/tests/mocks'
 import { makeFakeDatabase } from '@/tests/infra/database/postgres/mocks'
 import { PgRepository, ProductRepository } from '@/infra/database/postgres/repositories'
 import { PgConnection } from '@/infra/database/postgres/helpers'
 import { Category, Product } from '@/infra/database/postgres/entities'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
 
 import { IBackup, IMemoryDb } from 'pg-mem'
 import { Repository } from 'typeorm'
+import { mock } from 'jest-mock-extended'
 
 describe('ProductRepository', () => {
   let sut: ProductRepository
 
+  const { id: categoryId, name: categoryName } = categoryParams
   const { id, name, description, price } = productParams
 
   let connection: PgConnection
   let database: IMemoryDb
   let backup: IBackup
-  let repository: Repository<Product>
+  let repositoryCategory: Repository<Category>
+  let repositoryProduct: Repository<Product>
+
+  const uuid = mock<UUIDGenerator>()
 
   beforeAll(async () => {
     connection = PgConnection.getInstance()
     database = await makeFakeDatabase([Product, Category])
     backup = database.backup()
-    repository = connection.getRepository(Product)
+    repositoryCategory = connection.getRepository(Category)
+    repositoryProduct = connection.getRepository(Product)
+
+    uuid.generate.mockReturnValue(id)
   })
 
   beforeEach(() => {
     backup.restore()
 
-    sut = new ProductRepository()
+    sut = new ProductRepository(uuid)
   })
 
   it('Should extend PgRepository', async () => {
     expect(sut).toBeInstanceOf(PgRepository)
   })
 
-  it('Should return false if product does not exists', async () => {
-    const result = await sut.checkByName({ name })
+  describe('checkByName()', () => {
+    it('Should return false if product does not exists', async () => {
+      const result = await sut.checkByName({ name })
 
-    expect(result).toBe(false)
+      expect(result).toBe(false)
+    })
+
+    it('Should return true if product already exists', async () => {
+      await repositoryProduct.save({ id, name, description, price })
+
+      const result = await sut.checkByName({ name })
+
+      expect(result).toBe(true)
+    })
   })
 
-  it('Should return true if product already exists', async () => {
-    await repository.save({ id, name, description, price })
+  describe('create()', () => {
+    it('Should create a product on success', async () => {
+      await repositoryCategory.save({ id: categoryId, name: categoryName })
 
-    const result = await sut.checkByName({ name })
+      await sut.create({ categoryId, name, description, price })
 
-    expect(result).toBe(true)
+      expect(await repositoryProduct.findOne(id)).toBeTruthy()
+    })
   })
 })
