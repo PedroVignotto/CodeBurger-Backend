cc8312fa5d28d5a77a15da573d2b479bd1d37c96
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 01:50:16 -0300

    feat: add InvalidMimeTypeError validation

--- src/application/errors/validation.ts
+++ src/application/errors/validation.ts
@@ -11,3 +11,10 @@ export class InvalidFieldError extends Error {
     this.name = 'InvalidFieldError'
   }
 }
+
+export class InvalidMimeTypeError extends Error {
+  constructor (allowed: string[]) {
+    super(`Unsupported file. Allowed extensions: ${allowed.join(', ')}`)
+    this.name = 'InvalidMimeTypeError'
+  }
+}
