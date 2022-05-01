3df079f8b14df85dcea3b0bc0a44894f805935d9
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 17:11:58 -0300

    feat: ensure MulterAdapter add file to req.locals

--- src/main/adapters/multer.ts
+++ src/main/adapters/multer.ts
@@ -7,6 +7,8 @@ export const multerAdapter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
 
   upload(req, res, error => {
-    res.status(500).json({ error: new ServerError(error).message })
+    if (error) return res.status(500).json({ error: new ServerError(error).message })
+
+    if (req.file) req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
   })
 }
