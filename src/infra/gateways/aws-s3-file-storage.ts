e4fafd57215fa354a735f889c8e1920735469c95
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 00:47:36 -0300

    feat: ensure AwsS3FileStorage call putObject with correct values

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -1,8 +1,12 @@
-import { config } from 'aws-sdk'
+import { UploadFile } from '@/domain/contracts/gateways'
+import { config, S3 } from 'aws-sdk'
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AwsS3FileStorage {
-  constructor (accessKeyId: string, secretAccessKey: string) {
+  constructor (accessKeyId: string, secretAccessKey: string, private readonly bucket: string) {
     config.update({ credentials: { accessKeyId, secretAccessKey } })
+  }
+
+  async upload ({ fileName, file }: UploadFile.Input): Promise<void> {
+    await new S3().putObject({ Bucket: this.bucket, Key: fileName, Body: file, ACL: 'public-read' }).promise()
   }
 }
