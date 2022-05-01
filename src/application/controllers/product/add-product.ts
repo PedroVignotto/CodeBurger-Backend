fd9a10204f6b549502e2771188b935b5e63853a6
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-01 04:16:38 -0300

    feat: ensure AddProductController call addProduct with correct values

--- src/application/controllers/product/add-product.ts
+++ src/application/controllers/product/add-product.ts
@@ -1,12 +1,17 @@
 import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, created } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { AddProduct } from '@/domain/use-cases/product'
 
 type HttpRequest = { categoryId: string, name: string, description: string, price: number, file?: { buffer: Buffer, mimeType: string } }
 type Model = undefined | Error
 
 export class AddProductController extends Controller {
+  constructor (private readonly addProduct: AddProduct) { super() }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    await this.addProduct(httpRequest)
+
     return created(undefined)
   }
 
