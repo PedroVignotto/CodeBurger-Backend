a538cb97c00ad4eb24e83ae31aa97496efbbc147
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 02:25:36 -0300

    feat: ensure UpdateProductUseCase return NonExistentFieldError if CheckProductById return false

--- src/domain/use-cases/product/update-product.ts
+++ src/domain/use-cases/product/update-product.ts
@@ -1,10 +1,13 @@
 import { CheckProductByIdRepository } from '@/domain/contracts/database/repositories/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (productRepository: CheckProductByIdRepository) => UpdateProduct
 type Input = { id: string }
-type Output = void
+type Output = undefined | Error
 export type UpdateProduct = (input: Input) => Promise<Output>
 
 export const updateProductUseCase: Setup = productRepository => async ({ id }) => {
   await productRepository.checkById({ id })
+
+  return new NonExistentFieldError('id')
 }
