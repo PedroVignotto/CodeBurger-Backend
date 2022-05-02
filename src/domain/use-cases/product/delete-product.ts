6368ae5d255b7acd8991f9c3f479b748cbb453e3
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 19:28:58 -0300

    feat: ensure DeleteProductUseCase call DeleteProductRepository with correct id

--- src/domain/use-cases/product/delete-product.ts
+++ src/domain/use-cases/product/delete-product.ts
@@ -1,8 +1,8 @@
-import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { DeleteProductRepository, LoadProductRepository } from '@/domain/contracts/database/repositories/product'
 import { DeleteFile } from '@/domain/contracts/gateways'
 import { NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (productRepository: LoadProductRepository, fileStorage: DeleteFile) => DeleteProduct
+type Setup = (productRepository: LoadProductRepository & DeleteProductRepository, fileStorage: DeleteFile) => DeleteProduct
 type Input = { id: string }
 type Output = undefined | Error
 export type DeleteProduct = (input: Input) => Promise<Output>
@@ -13,4 +13,6 @@ export const deleteProductUseCase: Setup = (productRepository, fileStorage) => a
   if (!product) return new NonExistentFieldError('id')
 
   if (product.picture) await fileStorage.delete({ fileName: product.picture })
+
+  await productRepository.delete({ id })
 }
