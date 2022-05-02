95737d86d31b35b341a769034cf9d24ed8d3e2d9
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 01:16:33 -0300

    feat: ensure ListProductsController return ok if valid data is provided

--- src/application/controllers/product/list-products.ts
+++ src/application/controllers/product/list-products.ts
@@ -3,14 +3,14 @@ import { HttpResponse, ok } from '@/application/helpers'
 import { ListProducts } from '@/domain/use-cases/product'
 
 type HttpRequest = { categoryId: string }
-type Model = undefined
+type Model = Array<{ id: string, categoryId?: string, name: string, description: string, price: number, available: boolean, picture?: string }>
 
 export class ListProductsController extends Controller {
   constructor (private readonly listProducts: ListProducts) { super() }
 
   async perform ({ categoryId }: HttpRequest): Promise<HttpResponse<Model>> {
-    await this.listProducts({ categoryId })
+    const products = await this.listProducts({ categoryId })
 
-    return ok(undefined)
+    return ok(products)
   }
 }
