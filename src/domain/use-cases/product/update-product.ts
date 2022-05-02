10f28b69b786bc208b96f3b70ac093e713e3a276
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 04:54:47 -0300

    feat: ensure UpdateProductUseCase call UpdateProductRepository with correct values

--- src/domain/use-cases/product/update-product.ts
+++ src/domain/use-cases/product/update-product.ts
@@ -1,19 +1,27 @@
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
-import { CheckProductByIdRepository, CheckProductByNameRepository, LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { CheckProductByIdRepository, CheckProductByNameRepository, LoadProductRepository, UpdateProductRepository } from '@/domain/contracts/database/repositories/product'
 import { DeleteFile, UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (
-  productRepository: CheckProductByIdRepository & CheckProductByNameRepository & LoadProductRepository,
+  productRepository: CheckProductByIdRepository & CheckProductByNameRepository & LoadProductRepository & UpdateProductRepository,
   categoryRepository: CheckCategoryByIdRepository,
   fileStorage: UploadFile & DeleteFile,
   uuid: UUIDGenerator,
 ) => UpdateProduct
-type Input = { id: string, name?: string, categoryId?: string, file?: { buffer: Buffer, mimeType: string } }
+type Input = {
+  id: string
+  name?: string
+  categoryId?: string
+  description?: string
+  price?: number
+  available?: boolean
+  file?: { buffer: Buffer, mimeType: string }
+}
 type Output = undefined | Error
 export type UpdateProduct = (input: Input) => Promise<Output>
 
-export const updateProductUseCase: Setup = (productRepository, categoryRepository, fileStorage, uuid) => async ({ id, name, categoryId, file }) => {
+export const updateProductUseCase: Setup = (productRepository, categoryRepository, fileStorage, uuid) => async ({ id, name, categoryId, file, ...input }) => {
   const productNotExists = await productRepository.checkById({ id })
 
   if (!productNotExists) return new NonExistentFieldError('id')
@@ -30,13 +38,17 @@ export const updateProductUseCase: Setup = (productRepository, categoryRepositor
     if (!categoryExists) return new NonExistentFieldError('categoryId')
   }
 
-  if (file) {
-    const { picture } = await productRepository.load({ id })
+  let picture: string | undefined
 
-    if (picture) await fileStorage.delete({ fileName: picture })
+  if (file) {
+    const product = await productRepository.load({ id })
+
+    if (product.picture) await fileStorage.delete({ fileName: product.picture })
 
     const key = uuid.generate()
 
-    await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
+    picture = await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
   }
+
+  await productRepository.update({ id, categoryId, name, picture, ...input })
 }
