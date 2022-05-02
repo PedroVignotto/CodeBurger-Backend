89f505525db816663e79088c888aec81e6f454b2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 16:26:59 -0300

    feat: add UpdateProductController route

--- src/main/routes/product.ts
+++ src/main/routes/product.ts
@@ -1,5 +1,5 @@
 import { expressRouterAdapter as adapt, multerAdapter as upload } from '@/main/adapters'
-import { makeAddProductController, makeListProductsController } from '@/main/factories/application/controllers/product'
+import { makeAddProductController, makeListProductsController, makeUpdateProductController } from '@/main/factories/application/controllers/product'
 import { auth, authAdmin } from '@/main/middlewares'
 
 import { Router } from 'express'
@@ -7,4 +7,5 @@ import { Router } from 'express'
 export default (router: Router): void => {
   router.post('/product', authAdmin, upload, adapt(makeAddProductController()))
   router.get('/products', auth, adapt(makeListProductsController()))
+  router.put('/product/:id', authAdmin, upload, adapt(makeUpdateProductController()))
 }
