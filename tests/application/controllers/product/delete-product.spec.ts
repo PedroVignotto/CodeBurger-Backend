e48d30b7a645440d5dbc0c36d7c3685698a6aee3
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 19:41:59 -0300

    test: ensure DeleteProductController return badRequest if deleteProduct return NonExistentFieldError

--- tests/application/controllers/product/delete-product.spec.ts
+++ tests/application/controllers/product/delete-product.spec.ts
@@ -1,6 +1,7 @@
 import { productParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 import { DeleteProductController } from '@/application/controllers/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 describe('DeleteProductController', () => {
   let sut: DeleteProductController
@@ -22,5 +23,14 @@ describe('DeleteProductController', () => {
 
     expect(deleteProduct).toHaveBeenCalledWith({ id })
     expect(deleteProduct).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return badRequest if deleteProduct return NonExistentFieldError', async () => {
+    deleteProduct.mockResolvedValueOnce(new NonExistentFieldError('id'))
+
+    const { statusCode, data } = await sut.handle({ id })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new NonExistentFieldError('id'))
   })
 })
