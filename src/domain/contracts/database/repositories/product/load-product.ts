8551b4c15f36258cd2ea82639bbf3f6a283f22ce
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 02:55:12 -0300

    feat: add LoadProductRepository interface

---
+++ src/domain/contracts/database/repositories/product/load-product.ts
@@ -0,0 +1,16 @@
+export interface LoadProductRepository {
+  load: (input: LoadProductRepository.Input) => Promise<LoadProductRepository.Output>
+}
+
+export namespace LoadProductRepository {
+  export type Input = { id: string }
+  export type Output = {
+    id: string
+    categoryId?: string
+    name: string
+    description: string
+    price: number
+    available: boolean
+    picture?: string
+  }
+}
