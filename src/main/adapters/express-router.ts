73b491502fb8b0760c85efd28749fdbc3a75420c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 01:38:48 -0300

    feat: add query params on expressRouterAdapter

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,7 +5,7 @@ import { RequestHandler } from 'express'
 type Adapter = (controller: Controller) => RequestHandler
 
 export const expressRouterAdapter: Adapter = controller => async (req, res) => {
-  const { statusCode, data } = await controller.handle({ ...req.body, ...req.params, ...req.locals })
+  const { statusCode, data } = await controller.handle({ ...req.body, ...req.params, ...req.query, ...req.locals })
 
   const json = [200, 201, 204].includes(statusCode) ? data : { error: data.message }
 
