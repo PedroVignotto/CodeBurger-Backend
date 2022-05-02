19d6d59005d7e6f9f0f9cc455f3ce44780331759
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 02:26:05 -0300

    test: ensure UpdateProductUseCase return NonExistentFieldError if CheckProductById return false

--- tests/domain/use-cases/product/update-product.spec.ts
+++ tests/domain/use-cases/product/update-product.spec.ts
@@ -1,6 +1,7 @@
 import { productParams } from '@/tests/mocks'
 import { CheckProductByIdRepository } from '@/domain/contracts/database/repositories/product'
 import { UpdateProduct, updateProductUseCase } from '@/domain/use-cases/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -20,5 +21,13 @@ describe('UpdateProductUseCase', () => {
 
     expect(productRepository.checkById).toHaveBeenCalledWith({ id })
     expect(productRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return NonExistentFieldError if CheckProductByIdRepository return false', async () => {
+    productRepository.checkById.mockResolvedValueOnce(false)
+
+    const result = await sut({ id })
+
+    expect(result).toEqual(new NonExistentFieldError('id'))
   })
 })
