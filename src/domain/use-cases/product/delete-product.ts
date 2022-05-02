14a4051bbbef99453285ebe59b32bc3b0b065e04
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 19:02:15 -0300

    feat: ensure DeleteProductUseCase return NonExistentFieldError if LoadProduct return undefined

--- src/domain/use-cases/product/delete-product.ts
+++ src/domain/use-cases/product/delete-product.ts
@@ -1,10 +1,13 @@
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (productRepository: LoadProductRepository) => DeleteProduct
 type Input = { id: string }
-type Output = void
+type Output = Error
 export type DeleteProduct = (input: Input) => Promise<Output>
 
 export const deleteProductUseCase: Setup = productRepository => async ({ id }) => {
   await productRepository.load({ id })
+
+  return new NonExistentFieldError('id')
 }
