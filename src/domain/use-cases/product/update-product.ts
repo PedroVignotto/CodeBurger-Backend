bc64cd10dce0c34b1b1a6f3574d94904019b730f
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 02:39:06 -0300

    feat: ensure UpdateProductUseCase call CheckCategoryByIdRepository with correct id

--- src/domain/use-cases/product/update-product.ts
+++ src/domain/use-cases/product/update-product.ts
@@ -1,17 +1,24 @@
+import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByIdRepository, CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { FieldInUseError, NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (productRepository: CheckProductByIdRepository & CheckProductByNameRepository) => UpdateProduct
-type Input = { id: string, name: string }
+type Setup = (productRepository: CheckProductByIdRepository & CheckProductByNameRepository, categoryRepository: CheckCategoryByIdRepository) => UpdateProduct
+type Input = { id: string, name?: string, categoryId?: string }
 type Output = undefined | Error
 export type UpdateProduct = (input: Input) => Promise<Output>
 
-export const updateProductUseCase: Setup = productRepository => async ({ id, name }) => {
+export const updateProductUseCase: Setup = (productRepository, categoryRepository) => async ({ id, name, categoryId }) => {
   const productNotExists = await productRepository.checkById({ id })
 
   if (!productNotExists) return new NonExistentFieldError('id')
 
-  const productExists = await productRepository.checkByName({ name })
+  if (name) {
+    const productExists = await productRepository.checkByName({ name })
 
-  if (productExists) return new FieldInUseError('name')
+    if (productExists) return new FieldInUseError('name')
+  }
+
+  if (categoryId) {
+    await categoryRepository.checkById({ id: categoryId })
+  }
 }
