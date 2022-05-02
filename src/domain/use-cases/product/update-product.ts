1eabd5b5021d8269a812855b4d576d58b0d68765
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 04:20:36 -0300

    feat: ensure UpdateProductUseCase call UUIDGenerator

--- src/domain/use-cases/product/update-product.ts
+++ src/domain/use-cases/product/update-product.ts
@@ -1,18 +1,19 @@
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByIdRepository, CheckProductByNameRepository, LoadProductRepository } from '@/domain/contracts/database/repositories/product'
-import { DeleteFile } from '@/domain/contracts/gateways'
+import { DeleteFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (
   productRepository: CheckProductByIdRepository & CheckProductByNameRepository & LoadProductRepository,
   categoryRepository: CheckCategoryByIdRepository,
-  fileStorage: DeleteFile
+  fileStorage: DeleteFile,
+  uuid: UUIDGenerator,
 ) => UpdateProduct
 type Input = { id: string, name?: string, categoryId?: string, file?: { buffer: Buffer, mimeType: string } }
 type Output = undefined | Error
 export type UpdateProduct = (input: Input) => Promise<Output>
 
-export const updateProductUseCase: Setup = (productRepository, categoryRepository, fileStorage) => async ({ id, name, categoryId, file }) => {
+export const updateProductUseCase: Setup = (productRepository, categoryRepository, fileStorage, uuid) => async ({ id, name, categoryId, file }) => {
   const productNotExists = await productRepository.checkById({ id })
 
   if (!productNotExists) return new NonExistentFieldError('id')
@@ -33,5 +34,7 @@ export const updateProductUseCase: Setup = (productRepository, categoryRepositor
     const { picture } = await productRepository.load({ id })
 
     if (picture) await fileStorage.delete({ fileName: picture })
+
+    uuid.generate()
   }
 }
