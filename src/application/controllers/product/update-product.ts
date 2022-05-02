946a96fa925a6a6e373de728ae5c167352c054d2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 15:55:23 -0300

    feat: ensure UpdateProductController call updateProduct with correct values

--- src/application/controllers/product/update-product.ts
+++ src/application/controllers/product/update-product.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { UpdateProduct } from '@/domain/use-cases/product'
 
 type HttpRequest = {
   id: string
@@ -13,7 +14,11 @@ type HttpRequest = {
 }
 
 export class UpdateProductController extends Controller {
+  constructor (private readonly updateProduct: UpdateProduct) { super() }
+
   async perform (HttpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.updateProduct(HttpRequest)
+
     return noContent()
   }
 
