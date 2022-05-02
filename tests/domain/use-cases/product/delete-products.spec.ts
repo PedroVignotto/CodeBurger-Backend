49becf3d62174aa108a087d51a7aa9e71080a16b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 19:02:43 -0300

    test: ensure DeleteProductUseCase return NonExistentFieldError if LoadProduct return undefined

--- tests/domain/use-cases/product/delete-products.spec.ts
+++ tests/domain/use-cases/product/delete-products.spec.ts
@@ -1,6 +1,7 @@
 import { productParams } from '@/tests/mocks'
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
 import { DeleteProduct, deleteProductUseCase } from '@/domain/use-cases/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -20,5 +21,13 @@ describe('DeleteProductUseCase', () => {
 
     expect(productRepository.load).toHaveBeenCalledWith({ id })
     expect(productRepository.load).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return NonExistentFieldError if LoadProductRepository return undefined', async () => {
+    productRepository.load.mockResolvedValueOnce(undefined)
+
+    const result = await sut({ id })
+
+    expect(result).toEqual(new NonExistentFieldError('id'))
   })
 })
