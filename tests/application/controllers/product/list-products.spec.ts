311f852e34b6599c7e5d26226ee5749c7567d7d8
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-02 01:16:59 -0300

    test: ensure ListProductsController return ok if valid data is provided

--- tests/application/controllers/product/list-products.spec.ts
+++ tests/application/controllers/product/list-products.spec.ts
@@ -1,4 +1,4 @@
-import { categoryParams } from '@/tests/mocks'
+import { categoryParams, productParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 import { ListProductsController } from '@/application/controllers/product'
 
@@ -6,8 +6,13 @@ describe('ListProductsController', () => {
   let sut: ListProductsController
 
   const { id: categoryId } = categoryParams
+  const { id, name, description, price, available, picture } = productParams
 
   const listProducts: jest.Mock = jest.fn()
+
+  beforeAll(() => {
+    listProducts.mockResolvedValue([{ id, categoryId, name, description, price, available, picture }])
+  })
 
   beforeEach(() => {
     sut = new ListProductsController(listProducts)
@@ -22,5 +27,12 @@ describe('ListProductsController', () => {
 
     expect(listProducts).toHaveBeenCalledWith({ categoryId })
     expect(listProducts).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return ok if valid data is provided', async () => {
+    const { statusCode, data } = await sut.handle({ categoryId })
+
+    expect(statusCode).toBe(200)
+    expect(data).toEqual([{ id, categoryId, name, description, price, available, picture }])
   })
 })
