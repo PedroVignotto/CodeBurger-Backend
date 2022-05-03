9c1bc686a006ee0d07f73f3150075fa1bc4d4a43
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-03 07:27:13 -0300

    test: ensure ValidationBuilder return a Image validation if image() is call

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,4 +1,4 @@
-import { CompareValidation, EmailValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
+import { AllowedMimeTypesValidation, CompareValidation, EmailValidation, MaxFileSizeValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
 
 import faker from 'faker'
 
@@ -6,11 +6,13 @@ describe('ValidationBuilder', () => {
   let value: string
   let fieldName: string
   let fieldToCompare: string
+  let file: { buffer: Buffer, mimeType: string }
 
   beforeAll(() => {
     value = faker.database.column()
     fieldName = faker.database.column()
     fieldToCompare = faker.database.column()
+    file = { buffer: Buffer.from(faker.random.word()), mimeType: faker.system.commonFileExt() }
   })
 
   it('Should return a Required validation if required() is call', () => {
@@ -29,5 +31,11 @@ describe('ValidationBuilder', () => {
     const validators = Builder.of(value, fieldName).sameAs(fieldToCompare).build()
 
     expect(validators).toEqual([new CompareValidation(value, fieldToCompare, fieldName)])
+  })
+
+  it('Should return a Image validation if image() is call', () => {
+    const validators = Builder.of(file, fieldName).image({ AllowedMimeTypes: ['png'], maxSizeInMb: 5 }).build()
+
+    expect(validators).toEqual([new AllowedMimeTypesValidation(['png'], file.mimeType), new MaxFileSizeValidation(5, file.buffer)])
   })
 })
