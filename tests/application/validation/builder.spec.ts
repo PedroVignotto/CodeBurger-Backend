5942653681eceee44c760100194113adb11b8b15
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-03 07:15:43 -0300

    test: ensure ValidationBuilder return a Compare validation if sameAs() is call

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,14 +1,16 @@
-import { EmailValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
+import { CompareValidation, EmailValidation, RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
 
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   let value: string
   let fieldName: string
+  let fieldToCompare: string
 
   beforeAll(() => {
     value = faker.database.column()
     fieldName = faker.database.column()
+    fieldToCompare = faker.database.column()
   })
 
   it('Should return a Required validation if required() is call', () => {
@@ -21,5 +23,11 @@ describe('ValidationBuilder', () => {
     const validators = Builder.of(value, fieldName).email().build()
 
     expect(validators).toEqual([new EmailValidation(value, fieldName)])
+  })
+
+  it('Should return a Compare validation if sameAs() is call', () => {
+    const validators = Builder.of(value, fieldName).sameAs(fieldToCompare).build()
+
+    expect(validators).toEqual([new CompareValidation(value, fieldToCompare, fieldName)])
   })
 })
