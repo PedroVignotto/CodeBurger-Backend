bc162cf77cfdaf85318286eff0608a31b6a9f36d
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 17:54:47 -0300

    feat: ensure AddOrderUseCase call AddOrderRepository with correct values

--- src/domain/use-cases/order/add-order.ts
+++ src/domain/use-cases/order/add-order.ts
@@ -1,13 +1,16 @@
+import { AddOrderRepository } from '@/domain/contracts/database/repositories/order'
 import { LoadProductsRepository } from '@/domain/contracts/database/repositories/product'
 import { NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (productRepository: LoadProductsRepository) => AddOrder
-type Input = { productsId: string[] }
+type Setup = (productRepository: LoadProductsRepository, orderRepository: AddOrderRepository) => AddOrder
+type Input = { accountId: string, productsId: string[], note?: string, total: number, paymentMode: string }
 type Output = undefined | Error
 export type AddOrder = (input: Input) => Promise<Output>
 
-export const addOrderUseCase: Setup = productRepository => async ({ productsId }) => {
+export const addOrderUseCase: Setup = (productRepository, orderRepository) => async ({ accountId, productsId, note, total, paymentMode }) => {
   const products = await productRepository.loadAll({ productsId })
 
   if (products.length <= 0) return new NonExistentFieldError('productsId')
+
+  await orderRepository.create({ accountId, products, note, total, paymentMode })
 }
