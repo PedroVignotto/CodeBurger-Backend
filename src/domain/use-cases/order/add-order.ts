06b2669735b629e0d930ce1c995b9f884c26f677
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 17:44:30 -0300

    feat: ensure AddOrderUseCase return NonExistentFieldError if LoadProductsRepository return []

--- src/domain/use-cases/order/add-order.ts
+++ src/domain/use-cases/order/add-order.ts
@@ -1,10 +1,13 @@
 import { LoadProductsRepository } from '@/domain/contracts/database/repositories/product'
+import { NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (productRepository: LoadProductsRepository) => AddOrder
 type Input = { productsId: string[] }
-type Output = void
+type Output = undefined | Error
 export type AddOrder = (input: Input) => Promise<Output>
 
 export const addOrderUseCase: Setup = productRepository => async ({ productsId }) => {
-  await productRepository.loadAll({ productsId })
+  const products = await productRepository.loadAll({ productsId })
+
+  if (products.length <= 0) return new NonExistentFieldError('productsId')
 }
