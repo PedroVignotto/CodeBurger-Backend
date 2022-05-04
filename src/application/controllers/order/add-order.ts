1f0ea7049e8ea23933a0a6ef91a3d97f7d5bd605
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 18:47:42 -0300

    feat: ensure AddOrderController call addOrder with correct values

--- src/application/controllers/order/add-order.ts
+++ src/application/controllers/order/add-order.ts
@@ -1,12 +1,17 @@
 import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, created } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { AddOrder } from '@/domain/use-cases/order'
 
 type HttpRequest = { accountId: string, productsId: string[], note?: string, total: number, paymentMode: string }
 type Model = undefined | Error
 
 export class AddOrderController extends Controller {
+  constructor (private readonly addOrder: AddOrder) { super() }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    await this.addOrder(httpRequest)
+
     return created(undefined)
   }
 
