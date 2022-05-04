4552dd94be4558c866a3a560aa2948433f7c841c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 18:05:40 -0300

    feat: add Order entity

---
+++ src/infra/database/postgres/entities/order.ts
@@ -0,0 +1,38 @@
+import { Account } from './account'
+import { Product } from './product'
+
+import { Entity, Column, PrimaryColumn, ManyToOne, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable } from 'typeorm'
+
+@Entity('orders')
+export class Order {
+  @PrimaryColumn()
+  id!: string
+
+  @Column({ select: false })
+  accountId!: string
+
+  @ManyToOne(() => Account)
+  account!: Account
+
+  @ManyToMany(() => Product)
+  @JoinTable({ name: 'ordersProducts', joinColumns: [{ name: 'orderId' }], inverseJoinColumns: [{ name: 'productId' }] })
+  products!: Product[]
+
+  @Column({ nullable: true })
+  note!: string
+
+  @Column({ type: 'decimal' })
+  total!: number
+
+  @Column()
+  paymentMode!: string
+
+  @Column({ default: 'opened' })
+  status!: string
+
+  @CreateDateColumn()
+  createdAt!: Date
+
+  @UpdateDateColumn()
+  updatedAt!: Date
+}
