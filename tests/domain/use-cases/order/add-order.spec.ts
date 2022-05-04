ebea253f4cf7b534ed31062d2f38c8201c6e809c
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 17:45:17 -0300

    test: ensure AddOrderUseCase return NonExistentFieldError if LoadProductsRepository return []

--- tests/domain/use-cases/order/add-order.spec.ts
+++ tests/domain/use-cases/order/add-order.spec.ts
@@ -1,15 +1,21 @@
-import { productParams } from '@/tests/mocks'
+import { categoryParams, productParams } from '@/tests/mocks'
 import { LoadProductsRepository } from '@/domain/contracts/database/repositories/product'
 import { AddOrder, addOrderUseCase } from '@/domain/use-cases/order'
 
 import { mock } from 'jest-mock-extended'
+import { NonExistentFieldError } from '@/domain/errors'
 
 describe('AddOrderUseCase', () => {
   let sut: AddOrder
 
-  const { id: productId } = productParams
+  const { id: categoryId } = categoryParams
+  const { id: productId, name, description, price, available, picture } = productParams
 
   const productRepository = mock<LoadProductsRepository>()
+
+  beforeAll(() => {
+    productRepository.loadAll.mockResolvedValue([{ id: productId, categoryId, name, description, price, available, picture }])
+  })
 
   beforeEach(() => {
     sut = addOrderUseCase(productRepository)
@@ -20,5 +26,13 @@ describe('AddOrderUseCase', () => {
 
     expect(productRepository.loadAll).toHaveBeenCalledWith({ productsId: [productId] })
     expect(productRepository.loadAll).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return NonExistentFieldError if LoadProductsRepository return []', async () => {
+    productRepository.loadAll.mockResolvedValueOnce([])
+
+    const result = await sut({ productsId: [productId] })
+
+    expect(result).toEqual(new NonExistentFieldError('productsId'))
   })
 })
