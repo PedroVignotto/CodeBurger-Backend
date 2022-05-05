699318e908079280f33c9332f2475828efbf9464
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 02:21:17 -0300

    feat: ensure UpdateOrderController call updateOrder with correct values

--- src/application/controllers/order/update-order.ts
+++ src/application/controllers/order/update-order.ts
@@ -1,11 +1,16 @@
 import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { UpdateOrder } from '@/domain/use-cases/order'
 
 type HttpRequest = { id: string, status: string }
 
 export class UpdateOrderController extends Controller {
+  constructor (private readonly updateOrder: UpdateOrder) { super() }
+
   async perform ({ id, status }: HttpRequest): Promise<HttpResponse> {
+    await this.updateOrder({ id, status })
+
     return noContent()
   }
 
