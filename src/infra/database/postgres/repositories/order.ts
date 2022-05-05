1f96ef572c960618b05dafd8f102d8936ffc2051
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 23:43:00 -0300

    feat: ensure OrderRepository return all user orders

--- src/infra/database/postgres/repositories/order.ts
+++ src/infra/database/postgres/repositories/order.ts
@@ -1,9 +1,9 @@
 import { Order } from '@/infra/database/postgres/entities'
 import { PgRepository } from '@/infra/database/postgres/repositories'
 import { UUIDGenerator } from '@/domain/contracts/gateways'
-import { AddOrderRepository } from '@/domain/contracts/database/repositories/order'
+import { AddOrderRepository, ListOrdersRepository } from '@/domain/contracts/database/repositories/order'
 
-type Setup = AddOrderRepository
+type Setup = AddOrderRepository & ListOrdersRepository
 
 export class OrderRepository extends PgRepository implements Setup {
   constructor (private readonly uuid: UUIDGenerator) { super() }
@@ -12,5 +12,11 @@ export class OrderRepository extends PgRepository implements Setup {
     const repository = this.getRepository(Order)
 
     await repository.save({ id: this.uuid.generate(), accountId, products, paymentMode, total, note })
+  }
+
+  async list ({ accountId }: ListOrdersRepository.Input): Promise<ListOrdersRepository.Output> {
+    const repository = this.getRepository(Order)
+
+    return await repository.find({ where: { accountId }, relations: ['products'] })
   }
 }
