fc67ff0cc09d5e43b2ec2515b3834e5df156ca48
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 21:24:06 -0300

    test: ensure AddOrderUseCase return ValueNotExpected if total provided not match with price product

--- tests/domain/use-cases/order/add-order.spec.ts
+++ tests/domain/use-cases/order/add-order.spec.ts
@@ -1,21 +1,21 @@
 import { accountParams, categoryParams, orderParams, productParams } from '@/tests/mocks'
 import { LoadProductsRepository } from '@/domain/contracts/database/repositories/product'
+import { AddOrderRepository } from '@/domain/contracts/database/repositories/order'
+import { NonExistentFieldError, ValueNotExpectedError } from '@/domain/errors'
 import { AddOrder, addOrderUseCase } from '@/domain/use-cases/order'
 
 import { mock } from 'jest-mock-extended'
-import { NonExistentFieldError } from '@/domain/errors'
-import { AddOrderRepository } from '@/domain/contracts/database/repositories/order'
 
 describe('AddOrderUseCase', () => {
   let sut: AddOrder
 
   const { id: accountId } = accountParams
   const { id: categoryId } = categoryParams
-  const { id: productId, name, description, price, available, picture } = productParams
+  const { id: productId, name, description, available, picture } = productParams
   const { note, total, paymentMode, error } = orderParams
 
   const productsId = [productId]
-  const products = [{ id: productId, categoryId, name, description, price, available, picture }]
+  const products = [{ id: productId, categoryId, name, description, price: total, available, picture }]
 
   const productRepository = mock<LoadProductsRepository>()
   const orderRepository = mock<AddOrderRepository>()
@@ -49,6 +49,12 @@ describe('AddOrderUseCase', () => {
     const promise = sut({ accountId, productsId, note, total, paymentMode })
 
     await expect(promise).rejects.toThrow(error)
+  })
+
+  it('Should return ValueNotExpectedError if total provided not match with price products', async () => {
+    const result = await sut({ accountId, productsId, note, total: 300, paymentMode })
+
+    expect(result).toEqual(new ValueNotExpectedError('total'))
   })
 
   it('Should call AddOrderRepository with correct values', async () => {
