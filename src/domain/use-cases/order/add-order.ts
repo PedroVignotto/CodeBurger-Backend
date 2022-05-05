e0c0869d76a189ff3ceaa300c79f21ef34873da2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-04 21:23:06 -0300

    feat: ensure AddOrderUseCase return ValueNotExpected if total provided not match with price product

--- src/domain/use-cases/order/add-order.ts
+++ src/domain/use-cases/order/add-order.ts
@@ -1,6 +1,6 @@
 import { AddOrderRepository } from '@/domain/contracts/database/repositories/order'
 import { LoadProductsRepository } from '@/domain/contracts/database/repositories/product'
-import { NonExistentFieldError } from '@/domain/errors'
+import { NonExistentFieldError, ValueNotExpectedError } from '@/domain/errors'
 
 type Setup = (productRepository: LoadProductsRepository, orderRepository: AddOrderRepository) => AddOrder
 type Input = { accountId: string, productsId: string[], note?: string, total: number, paymentMode: string }
@@ -11,6 +11,10 @@ export const addOrderUseCase: Setup = (productRepository, orderRepository) => as
   const products = await productRepository.loadAll({ ids: productsId })
 
   if (products.length <= 0) return new NonExistentFieldError('productsId')
+
+  const sum = products.reduce((oldPrice, product) => oldPrice + product.price, 0)
+
+  if (sum !== total) return new ValueNotExpectedError('total')
 
   await orderRepository.create({ accountId, products, note, total, paymentMode })
 }
