10a58546dbd21180207e4cd18b6b06b80f291c14
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 00:07:48 -0300

    feat: ensure ListOrderController return ok if valid data is provided

--- src/application/controllers/order/list-orders.ts
+++ src/application/controllers/order/list-orders.ts
@@ -1,16 +1,17 @@
 import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, ok } from '@/application/helpers'
+import { Order } from '@/domain/contracts/database/repositories/order'
 import { ListOrders } from '@/domain/use-cases/order'
 
 type HttpRequest = { accountId: string }
-type Model = undefined
+type Model = Order[]
 
 export class ListOrderController extends Controller {
   constructor (private readonly listOrders: ListOrders) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse<Model>> {
-    await this.listOrders({ accountId })
+    const order = await this.listOrders({ accountId })
 
-    return ok(undefined)
+    return ok(order)
   }
 }
