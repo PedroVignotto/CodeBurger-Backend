00f8dce2f9de8f4c3664e3b06c1d7649aa5798fd
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 00:15:13 -0300

    feat: add ListOrders route

--- src/main/routes/order.ts
+++ src/main/routes/order.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeAddOrderController } from '@/main/factories/application/controllers/order'
+import { makeAddOrderController, makeListOrdersController } from '@/main/factories/application/controllers/order'
 import { auth } from '@/main/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/order', auth, adapt(makeAddOrderController()))
+  router.get('/orders', auth, adapt(makeListOrdersController()))
 }
