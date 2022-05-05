756f83da3c62da2aca0874391258b326f3a2e763
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 02:48:33 -0300

    feat: add UpdateOrderController route

--- src/main/routes/order.ts
+++ src/main/routes/order.ts
@@ -1,10 +1,11 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeAddOrderController, makeListOrdersController } from '@/main/factories/application/controllers/order'
-import { auth } from '@/main/middlewares'
+import { makeAddOrderController, makeListOrdersController, makeUpdateOrderController } from '@/main/factories/application/controllers/order'
+import { auth, authAdmin } from '@/main/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/order', auth, adapt(makeAddOrderController()))
   router.get('/orders', auth, adapt(makeListOrdersController()))
+  router.put('/order/:id', authAdmin, adapt(makeUpdateOrderController()))
 }
