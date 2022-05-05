34b2a8557d912e9ff9bf10de06749fda9789d3c2
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 01:13:42 -0300

    test: ensure UpdateOrderUseCase call UpdateOrderRepository with correct values

--- tests/domain/use-cases/order/update-order.spec.ts
+++ tests/domain/use-cases/order/update-order.spec.ts
@@ -1,5 +1,5 @@
 import { orderParams } from '@/tests/mocks'
-import { CheckOrderByIdRepository } from '@/domain/contracts/database/repositories/order'
+import { CheckOrderByIdRepository, UpdateOrderRepository } from '@/domain/contracts/database/repositories/order'
 import { UpdateOrder, updateOrderUseCase } from '@/domain/use-cases/order/update-order'
 
 import { mock } from 'jest-mock-extended'
@@ -8,16 +8,20 @@ import { NonExistentFieldError } from '@/domain/errors'
 describe('UpdateOrderUseCase', () => {
   let sut: UpdateOrder
 
-  const { id, error } = orderParams
+  const { id, error, status } = orderParams
 
-  const orderRepository = mock<CheckOrderByIdRepository>()
+  const orderRepository = mock<CheckOrderByIdRepository & UpdateOrderRepository>()
+
+  beforeAll(() => {
+    orderRepository.checkById.mockResolvedValue(true)
+  })
 
   beforeEach(() => {
     sut = updateOrderUseCase(orderRepository)
   })
 
   it('Should call CheckOrderByIdRepository with correct id', async () => {
-    await sut({ id })
+    await sut({ id, status })
 
     expect(orderRepository.checkById).toHaveBeenCalledWith({ id })
     expect(orderRepository.checkById).toHaveBeenCalledTimes(1)
@@ -26,7 +30,7 @@ describe('UpdateOrderUseCase', () => {
   it('Should return NonExistentFieldError if CheckOrderByIdRepository return false', async () => {
     orderRepository.checkById.mockResolvedValueOnce(false)
 
-    const result = await sut({ id })
+    const result = await sut({ id, status })
 
     expect(result).toEqual(new NonExistentFieldError('id'))
   })
@@ -34,8 +38,15 @@ describe('UpdateOrderUseCase', () => {
   it('Should rethrow if CheckOrderByIdRepository throws', async () => {
     orderRepository.checkById.mockRejectedValueOnce(error)
 
-    const promise = sut({ id })
+    const promise = sut({ id, status })
 
     await expect(promise).rejects.toThrow(error)
+  })
+
+  it('Should call UpdateOrderRepository with correct values', async () => {
+    await sut({ id, status })
+
+    expect(orderRepository.update).toHaveBeenCalledWith({ id, status })
+    expect(orderRepository.update).toHaveBeenCalledTimes(1)
   })
 })
