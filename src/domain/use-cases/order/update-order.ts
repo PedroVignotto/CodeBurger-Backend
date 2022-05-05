43247834c602a4b8ab971a9fc3bd16eefe7b5be7
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 01:04:16 -0300

    test: ensure UpdateOrderUseCase rethrow if CheckOrderByIdRepository throws

--- src/domain/use-cases/order/update-order.ts
+++ src/domain/use-cases/order/update-order.ts
@@ -1,10 +1,13 @@
 import { CheckOrderByIdRepository } from '@/domain/contracts/database/repositories/order'
+import { NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (orderRepository: CheckOrderByIdRepository) => UpdateOrder
 type Input = { id: string }
-type Output = void
+type Output = Error
 export type UpdateOrder = (input: Input) => Promise<Output>
 
 export const updateOrderUseCase: Setup = orderRepository => async ({ id }) => {
   await orderRepository.checkById({ id })
+
+  return new NonExistentFieldError('id')
 }
--- tests/domain/use-cases/order/update-order.spec.ts
+++ tests/domain/use-cases/order/update-order.spec.ts
@@ -3,11 +3,12 @@ import { CheckOrderByIdRepository } from '@/domain/contracts/database/repositori
 import { UpdateOrder, updateOrderUseCase } from '@/domain/use-cases/order/update-order'
 
 import { mock } from 'jest-mock-extended'
+import { NonExistentFieldError } from '@/domain/errors'
 
 describe('UpdateOrderUseCase', () => {
   let sut: UpdateOrder
 
-  const { id } = orderParams
+  const { id, error } = orderParams
 
   const orderRepository = mock<CheckOrderByIdRepository>()
 
@@ -20,5 +21,21 @@ describe('UpdateOrderUseCase', () => {
 
     expect(orderRepository.checkById).toHaveBeenCalledWith({ id })
     expect(orderRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return NonExistentFieldError if CheckOrderByIdRepository return false', async () => {
+    orderRepository.checkById.mockResolvedValueOnce(false)
+
+    const result = await sut({ id })
+
+    expect(result).toEqual(new NonExistentFieldError('id'))
+  })
+
+  it('Should rethrow if CheckOrderByIdRepository throws', async () => {
+    orderRepository.checkById.mockRejectedValueOnce(error)
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(error)
   })
 })
