31399cd5f41ffb1e207b57e21c9543714b99ecf8
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-05 01:13:15 -0300

    feat: ensure UpdateOrderUseCase call UpdateOrderRepository with correct values

--- src/domain/use-cases/order/update-order.ts
+++ src/domain/use-cases/order/update-order.ts
@@ -1,13 +1,15 @@
-import { CheckOrderByIdRepository } from '@/domain/contracts/database/repositories/order'
+import { CheckOrderByIdRepository, UpdateOrderRepository } from '@/domain/contracts/database/repositories/order'
 import { NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (orderRepository: CheckOrderByIdRepository) => UpdateOrder
-type Input = { id: string }
-type Output = Error
+type Setup = (orderRepository: CheckOrderByIdRepository & UpdateOrderRepository) => UpdateOrder
+type Input = { id: string, status: string }
+type Output = undefined | Error
 export type UpdateOrder = (input: Input) => Promise<Output>
 
-export const updateOrderUseCase: Setup = orderRepository => async ({ id }) => {
-  await orderRepository.checkById({ id })
+export const updateOrderUseCase: Setup = orderRepository => async ({ id, status }) => {
+  const order = await orderRepository.checkById({ id })
 
-  return new NonExistentFieldError('id')
+  if (!order) return new NonExistentFieldError('id')
+
+  await orderRepository.update({ id, status })
 }
