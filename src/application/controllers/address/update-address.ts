f2213dbde0eb0e6d47fa273d830298c35444025f
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 20:02:16 -0300

    feat: add active on address controller

--- src/application/controllers/address/update-address.ts
+++ src/application/controllers/address/update-address.ts
@@ -2,13 +2,13 @@ import { Controller } from '@/application/controllers/controller'
 import { HttpResponse, noContent } from '@/application/helpers'
 import { UpdateAddress } from '@/domain/use-cases/address'
 
-type HttpRequest = { accountId: string, id: string, surname?: string, number?: number, complement?: string }
+type HttpRequest = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
 
 export class UpdateAddressController extends Controller {
   constructor (private readonly updateAddress: UpdateAddress) { super() }
 
-  async perform ({ accountId, id, surname, number, complement }: HttpRequest): Promise<HttpResponse> {
-    await this.updateAddress({ accountId, id, surname, number, complement })
+  async perform ({ accountId, id, surname, number, complement, active }: HttpRequest): Promise<HttpResponse> {
+    await this.updateAddress({ accountId, id, surname, number, complement, active })
 
     return noContent()
   }
