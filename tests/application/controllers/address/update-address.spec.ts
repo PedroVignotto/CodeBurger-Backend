ed9b1d5d2fcc9d1898543764484430b911764d56
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 19:28:03 -0300

    test: ensure UpdateAddressUseCase call ListAddressesRepository with correct value

--- tests/application/controllers/address/update-address.spec.ts
+++ tests/application/controllers/address/update-address.spec.ts
@@ -1,4 +1,4 @@
-import { addressParams } from '@/tests/mocks'
+import { accountParams, addressParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 import { UpdateAddressController } from '@/application/controllers/address'
 import { NonExistentFieldError } from '@/domain/errors'
@@ -6,6 +6,7 @@ import { NonExistentFieldError } from '@/domain/errors'
 describe('UpdateAddressController', () => {
   let sut: UpdateAddressController
 
+  const { id: accountId } = accountParams
   const { id, surname, number, complement } = addressParams
 
   const updateAddress: jest.Mock = jest.fn()
@@ -23,23 +24,23 @@ describe('UpdateAddressController', () => {
   })
 
   it('Should call updateAddress with correct values', async () => {
-    await sut.handle({ id, surname, number, complement })
+    await sut.handle({ accountId, id, surname, number, complement })
 
-    expect(updateAddress).toHaveBeenCalledWith({ id, surname, number, complement })
+    expect(updateAddress).toHaveBeenCalledWith({ accountId, id, surname, number, complement })
     expect(updateAddress).toHaveBeenCalledTimes(1)
   })
 
   it('Should return badRequest if updateAddress return NonExistentFieldError', async () => {
     updateAddress.mockRejectedValueOnce(new NonExistentFieldError('id'))
 
-    const { statusCode, data } = await sut.handle({ id, surname, number, complement })
+    const { statusCode, data } = await sut.handle({ accountId, id, surname, number, complement })
 
     expect(statusCode).toBe(400)
     expect(data).toEqual(new NonExistentFieldError('id'))
   })
 
   it('Should return noContent if valid data is provided', async () => {
-    const { statusCode } = await sut.handle({ id, surname, number, complement })
+    const { statusCode } = await sut.handle({ accountId, id, surname, number, complement })
 
     expect(statusCode).toBe(204)
   })
--- tests/domain/use-cases/address/update-address.spec.ts
+++ tests/domain/use-cases/address/update-address.spec.ts
@@ -1,5 +1,5 @@
-import { addressParams } from '@/tests/mocks'
-import { CheckAddressByIdRepository, UpdateAddressRepository } from '@/domain/contracts/database/repositories/address'
+import { accountParams, addressParams } from '@/tests/mocks'
+import { CheckAddressByIdRepository, ListAddressesRepository, UpdateAddressRepository } from '@/domain/contracts/database/repositories/address'
 import { UpdateAddress, updateAddressUseCase } from '@/domain/use-cases/address'
 import { NonExistentFieldError } from '@/domain/errors'
 
@@ -8,9 +8,10 @@ import { mock } from 'jest-mock-extended'
 describe('UpdateAddressUseCase', () => {
   let sut: UpdateAddress
 
+  const { id: accountId } = accountParams
   const { id, surname, number, complement, error } = addressParams
 
-  const addressRepository = mock<CheckAddressByIdRepository & UpdateAddressRepository>()
+  const addressRepository = mock<CheckAddressByIdRepository & UpdateAddressRepository & ListAddressesRepository>()
 
   beforeAll(() => {
     addressRepository.checkById.mockResolvedValue(true)
@@ -21,7 +22,7 @@ describe('UpdateAddressUseCase', () => {
   })
 
   it('Should call CheckAddressByIdRepository with correct id', async () => {
-    await sut({ id })
+    await sut({ accountId, id })
 
     expect(addressRepository.checkById).toHaveBeenCalledWith({ id })
     expect(addressRepository.checkById).toHaveBeenCalledTimes(1)
@@ -30,7 +31,7 @@ describe('UpdateAddressUseCase', () => {
   it('Should throw NonExistentFieldError if CheckAddressByIdRepository return false', async () => {
     addressRepository.checkById.mockResolvedValueOnce(false)
 
-    const promise = sut({ id })
+    const promise = sut({ accountId, id })
 
     await expect(promise).rejects.toThrow(new NonExistentFieldError('id'))
   })
@@ -38,13 +39,20 @@ describe('UpdateAddressUseCase', () => {
   it('Should rethrow if CheckAddressByIdRepository throws', async () => {
     addressRepository.checkById.mockRejectedValueOnce(error)
 
-    const promise = sut({ id })
+    const promise = sut({ accountId, id })
 
     await expect(promise).rejects.toThrow(error)
   })
 
+  it('Should call ListAddressesRepository with correct value', async () => {
+    await sut({ accountId, id, surname, number, complement })
+
+    expect(addressRepository.list).toHaveBeenCalledWith({ accountId })
+    expect(addressRepository.list).toHaveBeenCalledTimes(1)
+  })
+
   it('Should call UpdateAddressRepository with correct values', async () => {
-    await sut({ id, surname, number, complement })
+    await sut({ accountId, id, surname, number, complement })
 
     expect(addressRepository.update).toHaveBeenCalledWith({ id, surname, number, complement })
     expect(addressRepository.update).toHaveBeenCalledTimes(1)
@@ -53,13 +61,13 @@ describe('UpdateAddressUseCase', () => {
   it('Should rethrow if UpdateAddressRepository throws', async () => {
     addressRepository.update.mockRejectedValueOnce(error)
 
-    const promise = sut({ id })
+    const promise = sut({ accountId, id })
 
     await expect(promise).rejects.toThrow(error)
   })
 
   it('Should return undefined on success', async () => {
-    const result = await sut({ id, surname, number, complement })
+    const result = await sut({ accountId, id, surname, number, complement })
 
     expect(result).toBeUndefined()
   })
