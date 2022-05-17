83062bb75c03e878ee6ee5c2d8061b269336fc5b
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 19:32:09 -0300

    feat: ensure UpdateAddressUseCase not call ListAddressesRepository if active is not provided

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -2,16 +2,16 @@ import { CheckAddressByIdRepository, ListAddressesRepository, UpdateAddressRepos
 import { NonExistentFieldError } from '@/domain/errors'
 
 type Setup = (addressRepository: CheckAddressByIdRepository & UpdateAddressRepository & ListAddressesRepository) => UpdateAddress
-type Input = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: string }
+type Input = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
 type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
 
-export const updateAddressUseCase: Setup = addressRepository => async ({ accountId, id, surname, number, complement }) => {
+export const updateAddressUseCase: Setup = addressRepository => async ({ accountId, id, surname, number, complement, active }) => {
   const address = await addressRepository.checkById({ id })
 
   if (!address) throw new NonExistentFieldError('id')
 
-  await addressRepository.list({ accountId })
+  if (active) await addressRepository.list({ accountId })
 
   await addressRepository.update({ id, surname, number, complement })
 }
