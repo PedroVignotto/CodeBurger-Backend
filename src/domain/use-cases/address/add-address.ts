6b715103fa363f0d71349ab9761077b120b9ca73
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 18:37:01 -0300

    feat: ensure AddAddressUseCase call ListAddressesRepository with correct value

--- src/domain/use-cases/address/add-address.ts
+++ src/domain/use-cases/address/add-address.ts
@@ -1,16 +1,18 @@
-import { AddAddressRepository } from '@/domain/contracts/database/repositories/address'
+import { AddAddressRepository, ListAddressesRepository } from '@/domain/contracts/database/repositories/address'
 import { SearchAddressByZipCode } from '@/domain/contracts/gateways'
 import { FieldNotFoundError } from '@/domain/errors'
 
-type Setup = (searchAddressByZipCode: SearchAddressByZipCode, addressRepository: AddAddressRepository) => AddAddress
+type Setup = (searchAddressByZipCode: SearchAddressByZipCode, addressRepository: AddAddressRepository & ListAddressesRepository) => AddAddress
 type Input = { accountId: string, surname: string, zipCode: string, district: string, street: string, number: number, complement?: string }
 type Output = { id: string, surname: string, zipCode: string, district: string, street: string, number: number, complement?: string }
 export type AddAddress = (input: Input) => Promise<Output>
 
-export const addAddressUseCase: Setup = (searchAddressByZipCode, addressRepository) => async ({ zipCode, ...input }) => {
+export const addAddressUseCase: Setup = (searchAddressByZipCode, addressRepository) => async ({ zipCode, accountId, ...input }) => {
   const address = await searchAddressByZipCode.search({ zipCode })
 
   if (!address) throw new FieldNotFoundError('zipCode')
 
-  return await addressRepository.create({ zipCode: zipCode.replace('-', ''), ...input })
+  await addressRepository.list({ accountId })
+
+  return await addressRepository.create({ accountId, zipCode: zipCode.replace('-', ''), ...input })
 }
