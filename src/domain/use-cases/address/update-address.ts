c68c8a099565f95029a1e253c3fa210a2c228feb
PedroVignotto <pedro.vignotto@hotmail.com>
2022-05-17 19:26:43 -0300

    feat: ensure UpdateAddressUseCase call ListAddressesRepository with correct value

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -1,15 +1,17 @@
-import { CheckAddressByIdRepository, UpdateAddressRepository } from '@/domain/contracts/database/repositories/address'
+import { CheckAddressByIdRepository, ListAddressesRepository, UpdateAddressRepository } from '@/domain/contracts/database/repositories/address'
 import { NonExistentFieldError } from '@/domain/errors'
 
-type Setup = (addressRepository: CheckAddressByIdRepository & UpdateAddressRepository) => UpdateAddress
-type Input = { id: string, surname?: string, number?: number, complement?: string }
+type Setup = (addressRepository: CheckAddressByIdRepository & UpdateAddressRepository & ListAddressesRepository) => UpdateAddress
+type Input = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: string }
 type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
 
-export const updateAddressUseCase: Setup = addressRepository => async ({ id, surname, number, complement }) => {
+export const updateAddressUseCase: Setup = addressRepository => async ({ accountId, id, surname, number, complement }) => {
   const address = await addressRepository.checkById({ id })
 
   if (!address) throw new NonExistentFieldError('id')
+
+  await addressRepository.list({ accountId })
 
   await addressRepository.update({ id, surname, number, complement })
 }
